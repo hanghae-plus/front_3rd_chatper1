9e42891bff6bcc9fe6b375a749470ab3bcc30dd9
junilhwang <junil.h@kakao.com>
2024-09-21 03:58:15 +0900

    fix: footer 추가

--- src/main.js
+++ src/main.js
@@ -102,6 +102,10 @@ document.querySelector('#root').innerHTML = `
           </div>
         </div>
       </main>
+
+      <footer class="bg-gray-200 p-4 text-center">
+        <p>&copy; 2024 항해플러스. All rights reserved.</p>
+      </footer>
     </div>
   </div>
 `;

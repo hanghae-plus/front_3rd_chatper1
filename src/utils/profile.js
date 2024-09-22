a5561bf29b925ea76c9b16a763cf9c7c4b3c8207
soyoonJ <sytkfkd96@gmail.com>
2024-09-22 19:53:20 +0900

    refactor: 프로필 관련 useProfile 분리

---
+++ src/utils/profile.js
@@ -0,0 +1,25 @@
+export const useProfile = () => {
+  const getProfile = () => {
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const bioInput = document.getElementById('bio');
+
+    const savedUser = JSON.parse(localStorage.getItem('user'));
+
+    usernameInput.value = savedUser?.username || '';
+    emailInput.value = savedUser?.email || '';
+    bioInput.value = savedUser?.bio || '';
+  };
+
+  const updateProfile = (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const bio = document.getElementById('bio').value;
+
+    localStorage.setItem('user', JSON.stringify({ username, email, bio }));
+  };
+
+  return { getProfile, updateProfile };
+};

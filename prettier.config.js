d8943dd45d4ec109bde7974b1740efe10247859f
chyerin802 <chyerin802@gmail.com>
2024-09-22 23:48:48 +0900

    chore: prettier 설치 및 규칙 설정

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+  semi: true, // 문장 끝에 세미콜론을 추가합니다.
+  singleQuote: true, // 문자열에 작은따옴표를 사용합니다.
+  trailingComma: 'es5', // ES5의 문법에서 허용되는 마지막 쉼표를 추가합니다.
+  arrowParens: 'always', // 화살표 함수의 매개변수에 괄호를 항상 추가합니다.
+  printWidth: 80, // 코드 한 줄의 최대 너비를 80자로 설정합니다.
+  tabWidth: 2, // 탭의 너비를 2로 설정합니다.
+  useTabs: true, // 탭으로 들여쓰기를 합니다.
+};

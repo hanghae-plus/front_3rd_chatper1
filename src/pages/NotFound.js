9c7f3b1973b4eaadaf0c44629f082ef269ccec31
soyoonJ <sytkfkd96@gmail.com>
2024-09-22 22:40:34 +0900

    fix: NotFound 홈으로 돌아가기 SPA routing으로 변경

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,24 @@
 import NotFoundPage from '../../templates/NotFoundPage';
+import { routes } from '../routes';
+import { useNavigate } from '../utils/navigate';
 
 export const NotFound = () => {
+  const { navigate } = useNavigate(routes);
+
   const render = () => {
     const rootElement = document.getElementById('root');
     rootElement.innerHTML = NotFoundPage;
+
+    addEvent();
+  };
+
+  const addEvent = () => {
+    document.querySelectorAll('a').forEach((anchor) => {
+      anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+        navigate(event.target.href);
+      });
+    });
   };
 
   render();

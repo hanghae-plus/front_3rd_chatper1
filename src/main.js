f9098b078c3201d863dd56b098cd41e164e99420
eggmun98 <eggmun98@gmail.com>
2024-09-22 22:50:15 +0900

    feat: 잘못 된 경로로 이동 시 에러 페이지로 이동 로직 구현

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { getHomeComponent } from "./components/pages/Home/Home";
 import { getInitComponent } from "./components/pages/Init/Init";
 import { getLoginComponent } from "./components/pages/Login/Login";
+import { getErrorComponent } from "./components/pages/Error/Error";
 
 document.querySelector("#root").innerHTML = getInitComponent();
 
@@ -10,8 +11,15 @@ document.getElementById("login").addEventListener("click", (e) => {
   document.querySelector("#main").innerHTML = getLoginComponent();
 });
 
-document.getElementById("main").addEventListener("click", (e) => {
+document.getElementById("home").addEventListener("click", (e) => {
   e.preventDefault();
-  history.pushState({ page: 2 }, "mainPage", "");
+  history.pushState({ page: 2 }, "mainPage", "/");
   document.querySelector("#main").innerHTML = getHomeComponent();
 });
+
+const currentPath = window.location.pathname;
+const isPaths = ["/login", "/"];
+if (!isPaths.includes(currentPath)) {
+  history.replaceState({ page: "error" }, "errorPage", "error");
+  document.querySelector("#main").innerHTML = getErrorComponent();
+}

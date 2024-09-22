b54575c90f35e21019dcd0a7dfe3e37613c00814
soyoonJ <sytkfkd96@gmail.com>
2024-09-22 19:55:00 +0900

    refactor: main.js 내 신규 생성 모듈 import - useNavigate, useAuth, useProfile

--- src/main.js
+++ src/main.js
@@ -3,86 +3,26 @@ import HomePage from '../templates/HomePage';
 import NotFoundPage from '../templates/NotFoundPage';
 import ProfilePage from '../templates/ProfilePage';
 
+import { useNavigate } from './utils/route';
+import { useAuth } from './utils/auth';
+import { useProfile } from './utils/profile';
+
 const routes = {
   '/': HomePage,
   '/login': LoginPage,
   '/profile': ProfilePage,
   '/404': NotFoundPage,
 };
-const protectedRoutes = ['/', '/profile'];
 
-const navigate = (path) => {
-  window.history.pushState({}, '', path);
-  updateHTML();
-};
+const navigate = useNavigate(routes);
+const { login, logout } = useAuth();
+const { getProfile, updateProfile } = useProfile();
 
-const isLoggedIn = () => {
-  const user = localStorage.getItem('user');
-  if (!user) return false;
-  return true;
-};
-
-const updateHTML = async () => {
-  const currentPath = window.location.pathname;
-  const targetHTML = routes[currentPath] || routes['/404'];
-
-  if (!isLoggedIn() && protectedRoutes.includes(currentPath)) {
-    navigate('/login');
-    return;
-  }
-
-  document.querySelector('#root').innerHTML = targetHTML;
-  document.querySelectorAll('a').forEach((anchor) => {
-    anchor.addEventListener('click', (event) => {
-      event.preventDefault();
-      navigate(event.target.href);
-    });
-  });
-
-  if (window.location.pathname === '/profile') {
-    getProfile();
-  }
-};
-
-const login = (event) => {
-  event.preventDefault();
-
-  const username = document.getElementById('username').value;
-  localStorage.setItem('user', JSON.stringify({ username, email: '', bio: '' }));
-
-  navigate('/profile');
-};
-const logout = () => {
-  localStorage.removeItem('user');
-  navigate('/login');
-};
-
-const getProfile = () => {
-  const usernameInput = document.getElementById('username');
-  const emailInput = document.getElementById('email');
-  const bioInput = document.getElementById('bio');
-
-  const savedUser = JSON.parse(localStorage.getItem('user'));
-
-  usernameInput.value = savedUser?.username || '';
-  emailInput.value = savedUser?.email || '';
-  bioInput.value = savedUser?.bio || '';
-};
-const updateProfile = (event) => {
-  event.preventDefault();
-
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const bio = document.getElementById('bio').value;
-
-  localStorage.setItem('user', JSON.stringify({ username, email, bio }));
-};
-
-window.addEventListener('popstate', updateHTML);
 // TODO: getElementById로 대체할 수 있는 지 확인
 window.addEventListener('submit', (event) => {
   if (event.target.id === 'login-form') {
     login(event);
+    navigate('/profile');
   }
 
   if (event.target.id === 'profile-form') {
@@ -92,10 +32,10 @@ window.addEventListener('submit', (event) => {
 window.addEventListener('click', (event) => {
   if (event.target.id === 'logout') {
     logout();
+    navigate('/login');
   }
 });
 
-updateHTML();
 if (window.location.pathname === '/profile') {
   getProfile();
 }

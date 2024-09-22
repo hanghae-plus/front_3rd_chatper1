dfc1dc6dc72b4378d4151fa017c473b143243382
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-22 23:16:14 +0900

    ✨ feat :: 푸터 컴포넌트 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 export default function header() {
   const user = JSON.parse(localStorage.getItem('user'));
   const currentPath = window.location.pathname;
-  
-  return `<header class="bg-blue-600 text-white p-4 sticky top-0">
+
+  return `
+  <header class="bg-blue-600 text-white p-4 sticky top-0">
     <h1 class="text-2xl font-bold">항해플러스</h1>
   </header>
 
@@ -12,5 +13,6 @@ export default function header() {
         ${user ? `<li><a href="/profile" class="${currentPath === '/profile' ? 'text-blue-600' : 'text-gray-600'}">프로필</a></li>
         <li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>` : `<li><a href="/login" class="${currentPath === '/login' ? 'text-blue-600' : 'text-gray-600'}">로그인</a></li>`}
       </ul>
-    </nav>`
+    </nav>
+    `;
 }

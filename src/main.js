8129a849c42666d6370c92423977644341237096
soyoonJ <sytkfkd96@gmail.com>
2024-09-23 23:22:20 +0900

    feat: 에러 감지 기능 추가

--- src/main.js
+++ src/main.js
@@ -5,3 +5,12 @@ const { updateHTML } = useNavigate(routes);
 
 window.addEventListener('popstate', updateHTML);
 updateHTML();
+
+window.addEventListener('error', (event) => {
+  document.getElementById('main').innerHTML = /* HTML */ `
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <strong class="font-bold">오류 발생!</strong>
+      <p>${event.message}</p>
+    </div>
+  `;
+});

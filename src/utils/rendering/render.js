6b887b431692b408e086365ea8c314540b1a3779
eggmun98 <eggmun98@gmail.com>
2024-09-23 19:43:34 +0900

    feat: 존재하지 않는 경로 시 error 페이지로 이동 기능 구현

--- src/utils/rendering/render.js
+++ src/utils/rendering/render.js
@@ -2,9 +2,10 @@ import {
   IS_PATCH,
   ROUTES,
 } from "../../components/contents/navigations/navigations";
+import { getErrorComponent } from "../../components/pages/Error/Error";
 
 export const render = (path) => {
-  const component = ROUTES[path] || ROUTES["/404"];
+  const component = ROUTES[path] || getErrorComponent;
   const container = IS_PATCH.includes(path) ? "#main" : "#root";
   document.querySelector(container).innerHTML = component();
 };

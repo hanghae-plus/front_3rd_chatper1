d56a11a73425a633e332420e2721f1b8d3c9c17f
onestone23 <lws2269@kakao.com>
2024-09-23 13:13:41 +0900

    feat: 컴포넌트, 페이지 구현

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+const Footer = () => `
+<footer class="bg-gray-200 p-4 text-center">
+  <p>&copy; 2024 항해플러스. All rights reserved.</p>
+</footer>`;
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = ({ selectPath, isLogged }) => {
+  return `
+  <header class="bg-blue-600 text-white p-4 sticky top-0">
+    <h1 class="text-2xl font-bold">항해플러스</h1>
+  </header>
+  <nav class="bg-white shadow-md p-2 sticky top-14">
+    <ul class="flex justify-around">
+      <li><a href="./main.html" class="text-blue-600">홈</a></li>
+      ${isLogged ? '<li><a href="./profile.html" class="text-gray-600">프로필</a></li>' : ''}
+      ${
+        isLogged
+          ? `<li>
+            <a href='#' class='text-gray-600'>
+              로그아웃
+            </a>
+          </li>`
+          : `<li>
+            <a href='./login.html' class='text-gray-600'>
+              로그인
+            </a>
+          </li>`
+      }
+    </ul>
+  </nav>`;
+};
+
+export default Header;

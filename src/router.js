14816c150d20441be90f89d939cdfac9d6c44ee0
김인영 <kiy7833@gmail.com>
2024-09-24 00:40:28 +0900

    fix: 이벤트 위임 테스트를 위한 라우트 가드 로직 수정

--- src/router.js
+++ src/router.js
@@ -17,13 +17,12 @@ export default function Router() {
   const render = (path) => {
     // 로그인 정보 체크
     const isUser = JSON.parse(localStorage.getItem("user"));
-    const isLoginPage = path === "/login";
 
-    if (isUser && isLoginPage) {
-      // 로그인 사용자가 로그인 페이지로 접근한 경우 홈 화면으로 전환
+    if (isUser && path === "/login") {
+      // 로그인 사용자가 로그인 페이지에 접근한 경우 홈 화면으로 전환
       navigateTo("/");
-    } else if (!isUser && !isLoginPage && routes[path]) {
-      // 비로그인 사용자가 로그인 페이지 외에 접근한 경우 로그인 페이지로 전환
+    } else if (!isUser && path === "/profile") {
+      // 비로그인 사용자가 프로필 페이지에 접근한 경우 로그인 페이지로 전환
       navigateTo("/login");
     } else {
       // 정상 접근 시 렌더링 && 비정상 URL 접근 시 404 화면

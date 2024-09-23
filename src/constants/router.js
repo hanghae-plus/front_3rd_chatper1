987f723178f9bb10c2134c9fb704deec34f5f2b4
안기환 <ahngh0113@naver.com>
2024-09-23 14:49:21 +0900

    자주 사용되는 문자 상수로 설정

---
+++ src/constants/router.js
@@ -0,0 +1,3 @@
+export const HOME_PAGE = "/";
+export const LOGIN_PAGE = "/login";
+export const PROFILE_PAGE = "/profile";
---
+++ src/constants/state.js
@@ -0,0 +1,5 @@
+export const STORAGE_NAME = "user";
+export const USERNAME = "username";
+export const EMAIL = "email";
+export const BIO = "bio";
+export const STOREAGE_KEYS = [USERNAME, EMAIL, BIO];

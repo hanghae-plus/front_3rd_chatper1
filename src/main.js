7757ee401c74f66f245a6f6a72586e055d2eaeb0
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-24 10:00:45 +0900

    로그인 ui 구현

--- src/main.js
+++ src/main.js
@@ -136,8 +136,21 @@ document.addEventListener("DOMContentLoaded", () => {
   function renderLogin() {
     document.title = "Login";
     document.querySelector("#root").innerHTML = `
-            <h1>Login Page</h1>
-           `;
+  <div class="bg-gray-100 min-h-screen flex justify-center">
+    <div class="max-w-md w-full">
+      <main class="p-4">
+       <div class="mb-4 bg-white rounded-lg shadow p-6 text-center">
+        <h1 class="text-2xl font-bold text-blue-600 mb-6">항해플러스</h1>
+         <input type="text" class="rounded-md w-full border p-2" placeholder="이메일 또는 전화번호" name="" id="" />
+         <input type="text" class="rounded-md w-full border p-2 my-4" placeholder="비밀번호" name="" id="" />
+         <button class="rounded-md w-full bg-blue-600 text-white py-2">로그인</button>
+         <p class="my-6 text-blue-500">비밀번호를 잊으셨나요?</p>
+         <div class="border border-gray-100"></div>
+         <button class="mt-6 text-white bg-green-600 px-3 py-2 rounded-md">새 계정 만들기</button>
+        </div>
+      </main>
+    </div>
+  </div>`;
 
     // // 로그인 폼 이벤트 리스너
     // document.getElementById("login-form").addEventListener("submit", (e) => {

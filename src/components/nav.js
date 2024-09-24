0ea009099517a1f2261b397d90986ee7b5754321
wktaylor <wktaylorla@gmail.com>
2024-09-24 23:39:33 +0900

    🚧 nav setup

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,33 @@
 import { html } from "lit-html";
+import { getUserInfo } from "../localStorage/user";
 
-const Nav = () => html`
-  <nav class="bg-white shadow-md p-2 sticky top-14">
-    <ul class="flex justify-around">
-      <li><a href="./main.html" class="text-gray-600">홈</a></li>
-      <li><a href="#" class="text-blue-600">프로필</a></li>
-      <li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>
-    </ul>
-  </nav>
+const NavLink = (href, text, isActive, id = "") => html`
+  <li>
+    <a
+      href="${href}"
+      class="${isActive ? "text-blue-600 font-bold" : "text-gray-600"}"
+      id="${id}"
+    >
+      ${text}
+    </a>
+  </li>
 `;
+
+const Nav = () => {
+  const isLogin = !!getUserInfo();
+  const path = window.location.pathname;
+
+  return html`
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+        ${NavLink("/", "홈", path === "/")}
+        ${NavLink("/profile", "프로필", path === "/profile")}
+        ${isLogin
+          ? NavLink("#", "로그아웃", false, "logout")
+          : NavLink("/login", "로그인", false)}
+      </ul>
+    </nav>
+  `;
+};
 
 export default Nav;

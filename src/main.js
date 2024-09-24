2fa82f180fa7f9d3976c2b5b67f48cc299fa192d
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-09-24 10:34:32 +0900

    프로필 업데이트 이벤트 추가

--- src/main.js
+++ src/main.js
@@ -196,22 +196,35 @@ document.addEventListener("DOMContentLoaded", () => {
       <main class="p-4">
         <div class="mb-4 bg-white rounded-lg shadow p-6 text-center">
         <h1 class="text-2xl font-bold text-blue-600 mb-6">내 프로필</h1>
-        <form id="login-form" class="text-left">
-          <label for="" c>사용자 이름 </label>
+        <form id="profile-form" class="text-left">
+          <label for="">사용자 이름 </label>
           <input id="username" type="text" class="rounded-md w-full border p-2 mb-6" value="${username}" />
-          <label for="" >이메일</label>
-          <input id="" type="text" class="rounded-md w-full border p-2 mb-6" placeholder="이메일" value="${email}"  />
-           <label for="">자기소개</label>
-          <textarea id="" class="rounded-md w-full border p-2 mb-6" placeholder="자기소개" />${introduction}</textarea>
+          <label  for="" >이메일</label>
+          <input id="email" type="text" class="rounded-md w-full border p-2 mb-6" placeholder="이메일" value="${email}"  />
+          <label for="">자기소개</label>
+          <textarea id="introduction" class="rounded-md w-full border p-2 mb-6" placeholder="자기소개" />${introduction}</textarea>
           <button class="rounded-md w-full bg-blue-600 text-white py-2">프로필 업데이트</button>
         </form>
       </main>
-
       <footer class="bg-gray-200 p-4 text-center">
         <p>&copy; 2024 항해플러스. All rights reserved.</p>
       </footer>
     </div>
   </div>`;
+
+    document.getElementById("profile-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      const username = document.getElementById("username").value;
+      const email = document.getElementById("email").value;
+      const introduction = document.getElementById("introduction").value;
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("email", email);
+      localStorage.setItem("introduction", introduction);
+
+      alert("프로필이 업데이트 되었습니다. ");
+      router();
+    });
   }
 
   function renderNotFound() {

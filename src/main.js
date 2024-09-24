265d7af63e0f64b3616ff28253ef8d307d36d94d
남균 <gyuni9604@naver.com>
2024-09-24 22:44:19 +0900

    ♻️ refactor : 상태관리 적용

--- src/main.js
+++ src/main.js
@@ -4,13 +4,23 @@ import LoginPage from "./pages/LoginPage.js"
 import ProfilePage from "./pages/ProfilePage.js"
 import NotFoundPage from "./pages/NotFoundPage.js"
 
+import loginStatus from "./utils/loginUtils.js"
+
+//초기화
+localStorage.removeItem("user")
+
+const root = document.querySelector("#root")
+
+// 로그인 상태 객체 생성
+const loginInfo = loginStatus()
+
 // 라우터 생성
 const router = createRouter()
 // 라우터에 라우트 추가
-router.addRoute("/", MainPage)
-router.addRoute("/login", LoginPage)
-router.addRoute("/profile", ProfilePage)
-router.addRoute("/404", NotFoundPage)
+router.addRoute("/", MainPage(loginInfo))
+router.addRoute("/login", LoginPage())
+router.addRoute("/profile", ProfilePage(loginInfo))
+router.addRoute("/404", NotFoundPage())
 router.addRoute(
   "/error",
   () => "<div>오류 발생! <br> 의도적인 오류입니다.</div>"
@@ -25,7 +35,11 @@ window.addEventListener("popstate", router.render)
 // 전역 에러 처리
 window.addEventListener("error", () => router.goTo("/error"))
 
-const root = document.querySelector("#root")
+// 클릭 이벤트 위임
+root.addEventListener("click", handleClick)
+
+// submit 이벤트 위임
+root.addEventListener("submit", handleSubmit)
 
 // 유저 정보 가져오기
 const userInfo = () => {
@@ -35,28 +49,25 @@ const userInfo = () => {
   return { username, email, bio }
 }
 
-// 클릭 이벤트 위임
-root.addEventListener("click", handleClick)
-
 // 클릭 이벤트 핸들러
 function handleClick(e) {
   const { tagName, id } = e.target
+
   // a 태그 클릭 시
   if (tagName === "A") {
     // 기본 동작 방지
     e.preventDefault()
+
     // 로그아웃 버튼 클릭 시
     if (id === "logout") {
-      ///localStorage 유저정보 삭제
-      localStorage.removeItem("user")
+      // 로그아웃 함수 호출
+      loginInfo.logout()
     }
+
     // 링크 클릭 시 goTo 함수 호출
     router.goTo(e.target.getAttribute("href"))
   }
 }
-
-// submit 이벤트 위임
-root.addEventListener("submit", handleSubmit)
 
 // submit 이벤트 핸들러
 function handleSubmit(e) {
@@ -66,14 +77,16 @@ function handleSubmit(e) {
   const user = userInfo()
 
   if (id === "login-form") {
+    // 유효성 검사
     if (!user.username) {
       alert("이메일 또는 전화번호 을(를) 입력해주세요.")
       return
     }
+    loginInfo.login(user)
   } else {
+    loginInfo.setUserInfo(user)
     alert("프로필이 업데이트 되었습니다.")
   }
-
-  localStorage.setItem("user", JSON.stringify(user))
+  // 로그인 함수 호출
   router.goTo("/profile")
 }

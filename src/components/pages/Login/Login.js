904c96a0e398c2d1f564b67af931f6082edaec0c
eggmun98 <eggmun98@gmail.com>
2024-09-24 21:04:14 +0900

    udpate: 로그인 폼의 상단 input id값 변경

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -4,7 +4,7 @@ export const getLoginComponent =
         <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
         <form id="login-form">
           <div class="mb-4">
-            <input type="text" id="email" placeholder="이메일 또는 전화번호" class="w-full p-2 border rounded">
+            <input type="text" id="username" placeholder="이메일 또는 전화번호" class="w-full p-2 border rounded">
           </div>
           <div class="mb-6">
             <input type="password" id="password" placeholder="비밀번호" class="w-full p-2 border rounded">

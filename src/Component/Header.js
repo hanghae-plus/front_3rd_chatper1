9d69ee57f8e8ac04180edcbc4de949a4ebe90540
Ensil <lasags@naver.com>
2024-09-24 19:07:43 +0900

    구현중) 컴포넌트 기반 구조설계

---
+++ src/Component/Header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+  
+    document.querySelector('#root').innerHTML = `
+        <header class="bg-blue-600 text-white p-4 sticky top-0">
+          <h1 class="text-2xl font-bold">항해플러스</h1>
+        </header>
+  `;
+};

a4423f19b2cb11a7ee9a46cf5deebaf7a9bdc18f
AaronYun <hyensooyoon@gmail.com>
2024-09-24 14:45:50 +0900

    [24/09/24] UPDATE utils.js

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,25 @@ export function removeUser() {
 export function isLogin() {
   return getUser() !== null;
 }
+
+// 태스트 해보기
+class User {
+    #user;
+
+    constructor() {
+        this.#user = JSON.parse(localStorage.getItem('user'));
+    }
+    
+    setUser(user) {
+        this.#user = user;
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+    getUser() {
+        return this.#user;
+    }
+    isLogin() {
+        return this.#user !== null;
+    }
+}
+
+// export default new User();

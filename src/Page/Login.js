4a0b5c150173a1d9f06c9b3973790e081369de77
Ensil <lasags@naver.com>
2024-09-24 19:06:27 +0900

    구현) 로그인 기능, 라우트 가드, 에러 바운더리

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,37 +1,7 @@
+import { goTo } from '../Util/util';
+
 export const Login = () => {
     console.log('Login Components!');
-
-    window.addEventListener('DOMContentLoaded', (event) => {
-        const loginForm = document.getElementById('login-form');
-
-        // 로그인 폼 제출 처리
-        loginForm.addEventListener('submit', function (e) {
-            e.preventDefault(); // 폼의 기본 제출 동작 막기
-            const usernameInput = document.getElementById('username').value;
-
-            // 유저 정보 저장
-            const user = {
-                username: usernameInput,
-                email: '',
-                bio: '',
-            };
-
-            localStorage.setItem('user', JSON.stringify(user));
-        });
-
-        // document.querySelector('#signup').addEventListener('click', handleSignUpButtonTapped);
-    });
-
-    // const handleSignUpButtonTapped = (e) => {
-    //     e.preventDefault();
-
-    //     const userInfo = { username: 'testuser', email: '', bio: '' };
-
-    //     localStorage.setItem('user', userInfo);
-
-    //     // window.history.pushState({}, '', 'signup');
-    //     // window.dispatchEvent(new Event('popstate'));
-    // };
 
     document.querySelector('#root').innerHTML = `
   <body>
@@ -41,10 +11,10 @@ export const Login = () => {
           <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
           <form id="login-form">
             <div class="mb-4">
-              <input type="text" placeholder="이메일 또는 전화번호" class="username w-full p-2 border rounded">
+              <input id="username" type="text" placeholder="이메일 또는 전화번호" class="username w-full p-2 border rounded">
             </div>
             <div class="mb-6">
-              <input type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
+              <input type="password" autocomplete="cc-number" placeholder="비밀번호" class="w-full p-2 border rounded">
             </div>
             <button type="submit" id="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">로그인</button>
           </form>
@@ -55,9 +25,73 @@ export const Login = () => {
           <div class="text-center">
             <button id="signup" class="bg-green-500 text-white px-4 py-2 rounded font-bold">새 계정 만들기</button>
           </div>
+          <div id="error-message"></div>
         </div>
       </main>
     </div>
   </body>
   `;
+
+    const isUserLogin = window.localStorage.getItem('user');
+
+    if (isUserLogin) {
+        goTo('/');
+        console.log('이미 로그인 되어 있어요!!', isUserLogin);
+    }
+
+    const loginForm = document.getElementById('login-form');
+
+    // 로그인 폼 제출 처리
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (e) {
+            e.preventDefault(); // 폼의 기본 제출 동작 막기
+            // 유저 정보 저장
+            const user = {
+                username: 'testuser',
+                email: '',
+                bio: '',
+            };
+
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+                goTo('/');
+            } catch (error) {
+                if (error instanceof FormValidationError) {
+                    document.getElementById(`${error.field}-error`).textContent = error.message;
+                } else {
+                    console.error('An unexpected error occurred:', error);
+                }
+            }
+        });
+    }
+
+    // throw new Error('의도적인 오류입니다.');
+
+    function setUsernameInputError() {
+        const usernameInput = document.getElementById('username');
+        if (usernameInput) {
+            usernameInput.addEventListener('input', (e) => {
+                e.preventDefault();
+
+                try {
+                    throw new Error('의도적인 오류입니다.');
+                } catch (error) {
+                    // Handle the error by calling the handleError function
+                    handleError(error);
+                }
+            });
+        }
+    }
+
+    function handleError(error) {
+        const errorDiv = document.getElementById('error-message');
+        errorDiv.innerHTML = `
+        <aside class="w-full p-8">
+          <h2 class="text-red-600 text-center">오류 발생!</h2>
+          <p class="text-center">${error.message}</p>
+        </aside>
+      `;
+    }
+
+    setUsernameInputError();
 };

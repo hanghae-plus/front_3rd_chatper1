575aa0dc94205fe2af97c12aed5973fcb72900c2
onestone23 <lws2269@kakao.com>
2024-09-24 15:59:07 +0900

    feat: header path 체크 -> bold

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 const Header = ({ isLogged }) => {
+  const checkSelectedPath = (path) => {
+    return window.location.pathname === path ? 'font-bold' : '';
+  };
+
   return `
   <header class="bg-blue-600 text-white p-4 sticky top-0">
     <h1 class="text-2xl font-bold">항해플러스</h1>
   </header>
   <nav class="bg-white shadow-md p-2 sticky top-14">
     <ul class="flex justify-around">
-      <li><a href="/" class="text-blue-600">홈</a></li>
+      <li><a href="/" class="text-blue-600 ${checkSelectedPath(
+        '/'
+      )}">홈</a></li>
       ${
         isLogged
-          ? '<li><a href="/profile" class="text-gray-600">프로필</a></li>'
+          ? `<li><a href="/profile" class="text-gray-600 ${checkSelectedPath(
+              '/profile'
+            )}">프로필</a></li>`
           : ''
       }
       ${

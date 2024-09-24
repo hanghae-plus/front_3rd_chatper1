f4848df8f33339236dbb65ab6c34a4e566b37a6f
남균 <gyuni9604@naver.com>
2024-09-24 22:40:52 +0900

    ♻️ refactor : 컴포넌트 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ const HeaderComponent = (isLogin) => {
 
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        <li><a href="/" class="text-blue-600 ${
-          homePage && "font-bold"
+        <li><a href="/" class=" ${
+          homePage ? "text-blue-600 font-bold" : "text-gray-600 font-bold"
         }">홈</a></li>
         ${
           isLogin
             ? `
-          <li><a href="/profile" class="text-gray-600 ${
-            !homePage && "font-bold"
+          <li><a href="/profile" id="login" class=" ${
+            !homePage ? "text-blue-600 font-bold" : "text-gray-600 font-bold"
           }" >프로필</a></li>
           <li><a href="/login" id="logout" class="text-gray-600">로그아웃</a></li>
           `
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 const LoginPage = () => {
-  return `
+  return () => `
   <main class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
       <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>

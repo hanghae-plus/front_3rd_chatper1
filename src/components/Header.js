bd25dca521d375de29f4d43995c60e3a4f6f4f66
soyoonJ <sytkfkd96@gmail.com>
2024-09-25 00:00:37 +0900

    refactor: Header 메뉴 return 구조 수정 - 기존: 하드코딩 - 변경: menus로 리스트 생성 후 map으로 관리

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,38 +3,32 @@ import { store } from '../store';
 const Header = (currentPath) => {
   const isLoggedIn = store.getState('isLoggedIn');
 
+  const menus = [
+    { name: '홈', path: '/', active: true },
+    { name: '프로필', path: '/profile', active: isLoggedIn },
+    { name: '로그인', path: '/login', active: !isLoggedIn },
+  ];
+
   const render = () => {
-    if (isLoggedIn)
-      return /* HTML */ `<header class="bg-blue-600 text-white p-4 sticky top-0">
-          <h1 class="text-2xl font-bold">항해플러스</h1>
-        </header>
+    return /* HTML */ `<header class="bg-blue-600 text-white p-4 sticky top-0">
+        <h1 class="text-2xl font-bold">항해플러스</h1>
+      </header>
 
-        <nav class="bg-white shadow-md p-2 sticky top-14">
-          <ul class="flex justify-around">
-            <li><a href="/" class="${currentPath === '/' ? 'text-blue-600 font-bold' : 'text-gray-600'}">홈</a></li>
-            <li>
-              <a href="/profile" class="${currentPath === '/profile' ? 'text-blue-600 font-bold' : 'text-gray-600'}"
-                >프로필</a
-              >
-            </li>
-            <li><button id="logout" class="text-gray-600">로그아웃</button></li>
-          </ul>
-        </nav>`;
-    else
-      return /* HTML */ `<header class="bg-blue-600 text-white p-4 sticky top-0">
-          <h1 class="text-2xl font-bold">항해플러스</h1>
-        </header>
-
-        <nav class="bg-white shadow-md p-2 sticky top-14">
-          <ul class="flex justify-around">
-            <li><a href="/" class="${currentPath === '/' ? 'text-blue-600 font-bold' : 'text-gray-600'}">홈</a></li>
-            <li>
-              <a href="/login" class="${currentPath === '/login' ? 'text-blue-600 font-bold' : 'text-gray-600'}"
-                >로그인</a
-              >
-            </li>
-          </ul>
-        </nav>`;
+      <nav class="bg-white shadow-md p-2 sticky top-14">
+        <ul class="flex justify-around">
+          ${menus
+            .map((menu) => {
+              if (!menu.active) return '';
+              return /* HTML */ `<li>
+                <a href=${menu.path} class="${currentPath === menu.path ? 'text-blue-600 font-bold' : 'text-gray-600'}"
+                  >${menu.name}</a
+                >
+              </li>`;
+            })
+            .join('')}
+          ${isLoggedIn ? '<li><button id="logout" class="text-gray-600">로그아웃</button></li>' : ''}
+        </ul>
+      </nav>`;
   };
 
   let headerHTML = render();

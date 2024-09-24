adc38729b4088b4f403464b9964eee1a6d8db68f
temeraire97 <hyensooyoon@gmail.com>
2024-09-25 00:48:06 +0900

    [24/09/25] UPDATE 변경된 user, router 사용

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,8 +1,9 @@
-import { getUser, setUser } from '../utils';
+import { user as userInstance } from '../utils';
+import router from '../router';
 
 class Profile {
   template() {
-    const user = getUser();
+    const user = userInstance.getUser();
 
     return `
     <main class="p-4">
@@ -11,15 +12,15 @@ class Profile {
         <form id="profile-form">
           <div class="mb-4">
             <label for="username" class="block text-gray-700 text-sm font-bold mb-2">사용자 이름</label>
-            <input type="text" id="username" name="username" value="${user.username}" class="w-full p-2 border rounded">
+            <input type="text" id="username" name="username" value="${user?.username}" class="w-full p-2 border rounded">
           </div>
           <div class="mb-4">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">이메일</label>
-            <input type="email" id="email" name="email" value="${user.email}" class="w-full p-2 border rounded">
+            <input type="email" id="email" name="email" value="${user?.email}" class="w-full p-2 border rounded">
           </div>
           <div class="mb-6">
             <label for="bio" class="block text-gray-700 text-sm font-bold mb-2">자기소개</label>
-            <textarea id="bio" name="bio" rows="4" class="w-full p-2 border rounded">${user.bio}</textarea>
+            <textarea id="bio" name="bio" rows="4" class="w-full p-2 border rounded">${user?.bio}</textarea>
           </div>
           <button type="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">프로필 업데이트</button>
         </form>
@@ -27,15 +28,15 @@ class Profile {
     </main>
     `;
   }
-  bindEvents(navigateTo) {
+  activeEvents() {
     document.getElementById('profile-form')?.addEventListener('submit', (e) => {
       const username = e.target.querySelector('input[type="text"]').value;
       const email = e.target.querySelector('input[type="email"]').value;
       const bio = e.target.querySelector('textarea[name="bio"]').value;
       console.log('bio', bio);
 
-      setUser({ username, email, bio });
-      navigateTo('/profile');
+      userInstance.setUser({ username, email, bio });
+      router.navigateTo('/profile');
       e.preventDefault();
     });
   }

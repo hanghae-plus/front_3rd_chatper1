c00bde206bd28e1f6ccac995b6281185360487ef
temeraire97 <hyensooyoon@gmail.com>
2024-09-25 00:44:29 +0900

    [24/09/25] UPDATE template 형태로 변경 및 컴포넌트를 직접 초기화 함

--- src/layouts/default_view.js
+++ src/layouts/default_view.js
@@ -1,30 +1,21 @@
-export default class DefaultView {
-  #navigation = '';
-  #content = '';
-  #footer = '';
+import Navigation from '../components/navigation';
+import Footer from '../components/footer';
 
-  constructor() {}
-
-  setNavigation(navigation) {
-    this.#navigation = navigation;
-  }
-  setContent(content) {
-    this.#content = content;
-  }
-  setFooter(footer) {
-    this.#footer = footer;
-  }
-
-  build() {
+class DefaultView {
+  template(content) {
     return `
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
-        
-        ${this.#navigation}
-        ${this.#content}
-        ${this.#footer}
+        ${Navigation.template()}
+        ${content.template()}
+        ${Footer.template()}
       </div>
     </div>
     `;
   }
+  activeEvents() {
+    Navigation.activeEvents();
+  }
 }
+
+export default new DefaultView();

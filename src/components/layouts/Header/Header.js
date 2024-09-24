8da28ab70c47f993f0a6122694aa38e4f31e1bd3
eggmun98 <eggmun98@gmail.com>
2024-09-24 23:28:11 +0900

    feat: Header 컴포넌트 유저 데이터에 따라서 분기처리 로직 구현

--- src/components/layouts/Header/Header.js
+++ src/components/layouts/Header/Header.js
@@ -1,4 +1,9 @@
-export const getHeaderComponent = (isLoginUser = false) => `
+import { getUser } from "../../../store/user/userStore";
+
+export const getHeaderComponent = (isLoginUser = false) => {
+  const user = getUser();
+
+  return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
         <h1 class="text-2xl font-bold">항해플러스</h1>
       </header>
@@ -7,7 +12,7 @@ export const getHeaderComponent = (isLoginUser = false) => `
         <ul class="flex justify-around">
         <li><a href="/" class="text-blue-600" id="home">홈</a></li>
         ${
-          isLoginUser
+          user
             ? `
           <li><a href="/profile" class="text-gray-600" id="profile">프로필</a></li>
           <li><a href="/logout" class="text-gray-600" id="logout">로그아웃</a></li>
@@ -25,3 +30,4 @@ export const getHeaderComponent = (isLoginUser = false) => `
       </nav>
 
 `;
+};

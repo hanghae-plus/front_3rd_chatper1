1e71870b8c4c85a5761e7bffc2ecc987835c9d46
Eunsu Lim <limes1787@gmail.com>
2024-09-26 00:09:08 +0900

    feat: page 분리, component 파일 추가

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export function Footer() {
+    return `
+            <footer class="bg-gray-200 p-4 text-center">
+                <p>&copy; 2024 항해플러스. All rights reserved.</p>
+            </footer>
+        `;
+  }
+  

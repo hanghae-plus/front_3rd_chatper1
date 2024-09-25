a9e4a4f9f2e24f58f94a9deabf85f6e55171ee3d
Bong <borobong230@gmail.com>
2024-09-26 01:34:19 +0900

    :recycle: [Refactor] 라우터 관련 함수 분리

---
+++ src/router.js
@@ -0,0 +1,44 @@
+import LoginPage from "./pages/Login.js";
+import HomePage from "./pages/Home.js";
+import ProfilePage from "./pages/Profile.js";
+import NotFoundPage from "./pages/NotFound.js";
+import Layout from "./layout/Layout.js";
+
+const renderPage = (path) => {
+  const currentRoute = path || window.location.pathname;
+  let currentPage;
+
+  switch (currentRoute) {
+    case "/":
+      currentPage = Layout(HomePage());
+      break;
+
+    case "/login":
+      if (localStorage.getItem("user")) {
+        return navigate("/");
+      }
+      currentPage = LoginPage();
+      break;
+
+    case "/profile":
+      if (!localStorage.getItem("user")) {
+        currentPage = LoginPage();
+      } else {
+        currentPage = Layout(ProfilePage());
+      }
+      break;
+
+    default:
+      currentPage = NotFoundPage();
+  }
+
+  document.querySelector("#root").innerHTML = currentPage;
+  handleTabClick();
+};
+
+const navigate = (path) => {
+  window.history.pushState({}, "", path);
+  renderPage(path);
+};
+
+export { renderPage, navigate };

41251ce863583e89228ac3510b3d52b6109dbb15
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-09-25 21:48:26 +0900

    헤더 nav 클릭했을때 해당 페이지 이동 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-import { router } from "./router";
+import { navigateTo, router } from "./router";
 
 window.addEventListener("popstate", router);
-// DOM이 로드되면 router 실행.
-document.addEventListener("DOMContentLoaded", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.classList.contains("home")) {
+      e.preventDefault();
+      navigateTo("/");
+    } else if (e.target.classList.contains("profile")) {
+      e.preventDefault();
+      navigateTo("/profile");
+    } else if (e.target.classList.contains("login")) {
+      e.preventDefault();
+      navigateTo("/login");
+    }
+  });
+
+  router();
+});

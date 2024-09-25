1de020afa1a0b9c3ec7462abc3ab1a03218bc99c
iammiori <immioriii@gmail.com>
2024-09-26 03:58:05 +0900

    feat: add router

---
+++ src/router.js
@@ -0,0 +1,62 @@
+import UserInfo from './userInfo';
+
+export class Router {
+  static routes = [];
+
+  static initialize(routes) {
+    this.routes = routes;
+
+    this.handleRoute(location.pathname);
+
+    this.setupEventListeners();
+  }
+
+  static setupEventListeners() {
+    window.addEventListener('popstate', () => {
+      this.handleRoute(location.pathname);
+    });
+
+    document.addEventListener('click', (event) => {
+      const target = event.target.closest('a');
+
+      if (!target) {
+        return;
+      }
+      if (target.href) {
+        event.preventDefault();
+        this.handleLinkClick(target.pathname);
+      }
+
+      if (target.id === 'logout') {
+        UserInfo.clear();
+        this.redirect('/login');
+      }
+    });
+  }
+
+  static handleLinkClick(newPath) {
+    if (newPath !== location.pathname) {
+      history.pushState(null, '', newPath);
+    }
+    this.handleRoute(newPath);
+  }
+
+  static handleRoute(path) {
+    if (path === '/profile' && !UserInfo.get('username')) {
+      this.redirect('/login');
+      return;
+    }
+
+    const route =
+      this.routes.find((route) => route.path === path) ||
+      this.routes.find((route) => route.path === '/404');
+
+    const view = new route.view();
+    view.render();
+  }
+
+  static redirect(newPath) {
+    history.pushState(null, '', newPath);
+    this.handleRoute(newPath);
+  }
+}

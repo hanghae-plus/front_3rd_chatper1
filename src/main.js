8e3c007620fc8cbe19b98ace003e4492ecf8c8d6
Bong <borobong230@gmail.com>
2024-09-26 00:45:07 +0900

    :sparkles: [Feat] 심화 과제 - 로그인 의도치 않은 에러처리 기능 추가 #main

--- src/main.js
+++ src/main.js
@@ -108,3 +108,16 @@ window.addEventListener("popstate", () => {
 document.addEventListener("DOMContentLoaded", () => {
   renderPage();
 });
+
+window.addEventListener("error", (event) => {
+  document.querySelector("#root").innerHTML = /* HTML */ `
+    <div
+      class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      class=""
+      role="alert"
+    >
+      <strong class="font-bold block">오류 발생!</strong>
+      <p class="block sm:inline">${event.message}</p>
+    </div>
+  `;
+});

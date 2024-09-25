50be04db6146c04cad377ca525966936d53eab84
김인영 <kiy7833@gmail.com>
2024-09-25 23:47:30 +0900

    refactor: 인증 모듈에서는 로그인 여부 체크 함수만 두도록 수정

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,19 +1,14 @@
-// 사용자 정보 조회
-export const getUserInfo = () => {
-  return JSON.parse(localStorage.getItem("user"));
-};
-
 // 로컬 스토리지에서 사용자 정보를 가져와 로그인 여부 확인
 export const isAuthenticated = () => {
-  return !!getUserInfo();
+  return localStorage.getItem("isLoggedIn");
 };
 
-// 로컬 스토리지에 사용자 정보 저장 (로그인 처리)
-export const setUserInfo = (user) => {
-  localStorage.setItem("user", JSON.stringify(user));
+export const login = () => {
+  localStorage.setItem("isLoggedIn", true);
 };
 
 // 로컬 스토리지에서 사용자 정보 삭제 (로그아웃 처리)
 export const logout = () => {
+  localStorage.removeItem("isLoggedIn");
   localStorage.removeItem("user");
 };

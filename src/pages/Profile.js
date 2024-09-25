ad53896bbb279bcbc6e0da4f9ac369be8606ee86
Bong <borobong230@gmail.com>
2024-09-25 18:21:16 +0900

    :hammer: [Update] href 태그 수정(.html => /[페이지명]) #main

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,9 +7,11 @@ const Profile = () => {
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="./main.html" class="text-gray-600">홈</a></li>
-          <li><a href="#" class="text-blue-600">프로필</a></li>
-          <li><a href="#" class="text-gray-600">로그아웃</a></li>
+          <li><a href="/" class="text-gray-600">홈</a></li>
+          <li><a href="/profile" class="text-blue-600">프로필</a></li>
+          <li>
+            <a href="/login" class="text-gray-600" id="logout">로그아웃</a>
+          </li>
         </ul>
       </nav>
 

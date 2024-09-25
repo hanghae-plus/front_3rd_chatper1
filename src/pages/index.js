6842306e3f0646fceb7242c6fd6c6a072eb95d9a
chyerin802 <chyerin802@gmail.com>
2024-09-25 09:14:28 +0900

    refactor: 페이지 컴포넌트 named export

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import NotFoundPage from './NotFoundPage';
+import ProfilePage from './ProfilePage';
+
+export { HomePage, LoginPage, NotFoundPage, ProfilePage };

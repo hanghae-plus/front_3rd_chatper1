b452f3c99c6f3d7eaeb9fab0e5a20253fee6251e
Bong <borobong230@gmail.com>
2024-09-26 02:13:00 +0900

    :sparkles: [Feat] 유저 스토어 추가

---
+++ src/store/user.js
@@ -0,0 +1,20 @@
+export const user = () => {
+  const getUser = () => JSON.parse(localStorage.getItem("user")) || null;
+
+  const setUser = (user) => {
+    const isInvalidUser = !user || typeof user !== "object";
+    if (isInvalidUser) {
+      throw new Error("Invalid user object");
+    }
+
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const removeUser = () => {
+    localStorage.removeItem("user");
+  };
+
+  const isLoggedIn = () => !!getUser();
+
+  return { getUser, setUser, removeUser, isLoggedIn };
+};

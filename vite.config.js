89d59321e907aa5292b72e4f1d91fcaba48d6ca1
KimDanwoo <dudgh9198@naver.com>
2024-09-25 17:46:46 +0900

    chore: vite alias 변경

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@routers': path.resolve(__dirname, './src/routers'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
       '@pages': path.resolve(__dirname, './src/pages'),
+      '@routers': path.resolve(__dirname, './src/routers'),
+      '@services': path.resolve(__dirname, './src/services'),
       '@stores': path.resolve(__dirname, './src/stores'),
       '@utils': path.resolve(__dirname, './src/utils'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@constants': path.resolve(__dirname, './src/constants'),
     },
   },
 })

32d052c64bb1ab1416a5b3bbc2de4516cf38f575
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-09-25 21:07:35 +0900

    form submit handler 추가 - 로그인 기능

---
+++ src/formSubmitHandler.js
@@ -0,0 +1,36 @@
+import { navigateTo } from "./router";
+
+export const formSubmitHandler = (
+  form,
+  nameInput,
+  passwordInput,
+  loginButton
+) => {
+  const checkInputTrim = () => {
+    // 입력 필드 값 가져오기
+    const isUserName = nameInput.value.trim() !== "";
+    const isUserPassword = passwordInput.value.trim() !== "";
+
+    console.log("isUserName:", isUserName);
+    console.log("isUserPassword", isUserPassword);
+
+    if (isUserName && isUserPassword) {
+      loginButton.removeAttribute("disabled");
+    } else {
+      loginButton.setAttribute("disabled", "true");
+    }
+  };
+
+  nameInput.addEventListener("change", checkInputTrim);
+  passwordInput.addEventListener("change", checkInputTrim);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); // 폼 제출 시 페이지 리로드 방지
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify({ username: nameInput.value, email: "", bio: "" })
+    );
+    navigateTo("/");
+  });
+};

af61d2fdd9035bc7d568d691381c402cd55cc2ad
Bong <borobong230@gmail.com>
2024-09-25 19:53:37 +0900

    :recycle: [Refactor] Footer.js 생성(분리) #main

---
+++ src/layout/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return /* HTML */ `<footer class="bg-gray-200 p-4 text-center">
+    <p>&copy; 2024 항해플러스. All rights reserved.</p>
+  </footer>`;
+};
+
+export default Footer;

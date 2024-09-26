f6bc5c44547b9c29d3646813ab671131556d80cf
temeraire97 <hyensooyoon@gmail.com>
2024-09-26 20:52:13 +0900

    [24/09/26] UPDATE

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -10,6 +10,7 @@ class Navigation {
     <header class="bg-blue-600 text-white p-4 sticky top-0">
       <h1 class="text-2xl font-bold">항해플러스</h1>
     </header>
+
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
         <li><a href="/" class="${isActivated('/')}">홈</a></li>

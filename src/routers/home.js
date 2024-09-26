121b5a96b77aa172f84f4abea9a51e39d409dc42
Kang Seokhyun <yoo2yoo2yoo2@gmail.com>
2024-09-26 20:05:11 +0900

    ✨심화과제구현

--- src/routers/home.js
+++ src/routers/home.js
@@ -7,7 +7,7 @@ const component = `
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
+          <li><a href="/" class="text-blue-600 font-bold">홈</a></li>
           <li><a href="/profile" class="text-gray-600">프로필</a></li>
           <li><a id="logout" href="#" class="text-gray-600">로그아웃</a></li>
         </ul>

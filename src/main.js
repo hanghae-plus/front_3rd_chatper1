919ab8385b474ab9d3c7d97380c3ecaebe74677b
seungwoo Kim <kyh196201@gmail.com>
2024-09-26 22:49:06 +0900

    feat: 심화 테스트 3번 구현

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import router from './router';
 const init = () => {
   router.init();
 
+  window.addEventListener('error', (e) => {
+    const errorElement = document.createElement('div');
+    errorElement.innerHTML = `
+      <p>오류 발생!</p>
+      <p>${e.message}</p>
+    `.trim();
+    document.body.appendChild(errorElement);
+  });
+  // window.onerror = function (msg, url, lineNo, columnNo, error) {
+
+  //   // https://stackoverflow.com/questions/10825992/prevent-javascript-from-stopping-when-error-is-encountered
+  //   // return false 일 경우 uncaught 에러로 인해 앱이 망가짐
+  //   // or addEventListener('error', (e) => { e.preventDefault })
+  //   return true;
+  // };
+
   document.addEventListener('click', (e) => {
     if (e.target.closest('a.link')) {
       e.preventDefault();

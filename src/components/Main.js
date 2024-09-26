d22bc72db35400448241a4e53e4afab4d59373bd
onfleek <onfleek@onfleekui-MacBookPro.local>
2024-09-27 00:33:04 +0900

    현 상황 리드미 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,7 @@ export default function Main() {
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="./main" class="text-blue-600">홈</a></li>
+          <li><a href="./main" class="text-blue-600 font-bold">홈</a></li>
           <li><a href="./profile" class="text-gray-600">프로필</a></li>
           <li><a href="#" class="text-gray-600">로그아웃</a></li>
         </ul>

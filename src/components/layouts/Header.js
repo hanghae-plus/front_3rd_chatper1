732dc98d8445a389918115513ad5ba301191b21e
KimDanwoo <dudgh9198@naver.com>
2024-09-26 13:59:00 +0900

    refactor: header 리팩토링

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,4 +1,4 @@
-import { PAGE_TITLE } from '@constants'
+import { PROJECT_NAME } from '@constants'
 import { UserStore } from '@stores'
 import { ROUTES } from '@constants'
 
@@ -12,49 +12,51 @@ const NAVIGATION = {
 
 export default function header() {
   let pathname = location.pathname
-  const isActive = (path) => (pathname === path ? 'text-blue-600 font-bold' : 'text-gray-600')
+  const isActive = (path) => {
+    return pathname === path ? 'text-blue-600 font-bold' : 'text-gray-600'
+  }
 
   const isLogin = userStore.getState('isLogin')
 
-  const navList = [
+  const navigationList = [
     {
       name: NAVIGATION.HOME.KOR,
       link: ROUTES.HOME,
       id: NAVIGATION.HOME.ENG,
-      condition: () => true,
+      condition: true,
     },
     {
       name: NAVIGATION.PROFILE.KOR,
       link: ROUTES.PROFILE,
       id: NAVIGATION.PROFILE.ENG,
-      condition: () => isLogin,
+      condition: isLogin,
     },
     {
       name: NAVIGATION.LOGIN.KOR,
       link: ROUTES.LOGIN,
       id: NAVIGATION.LOGIN.ENG,
-      condition: () => !isLogin,
+      condition: !isLogin,
     },
     {
       name: NAVIGATION.LOGOUT.KOR,
       link: ROUTES.LOGIN,
       id: NAVIGATION.LOGOUT.ENG,
-      condition: () => isLogin,
+      condition: isLogin,
     },
   ]
   return `<div>
     <header class="bg-blue-600 text-white p-4 sticky top-0">
-      <h1 class="text-2xl font-bold">${PAGE_TITLE}</h1>
+      <h1 class="text-2xl font-bold">${PROJECT_NAME}</h1>
     </header>
 
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        ${navList
-          .filter((item) => item.condition())
+        ${navigationList
+          .filter((item) => item.condition)
           .map(
-            (item) => `<li>
-                <a id="${item.id}" href="${item.link}" class="${isActive(item.link)}">${item.name}</a>
-              </li>`
+            ({ id, link, name }) => `<li>
+              <a id="${id}" href="${link}" class="${isActive(link)}">${name}</a>
+            </li>`
           )
           .join('')} 
       </ul>

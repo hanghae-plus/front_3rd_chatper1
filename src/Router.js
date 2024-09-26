e21dcbd7b12bee31b030625a376fc44270bdadb5
Ben(석금용) <gmyong21@gmail.com>
2024-09-26 22:48:27 +0900

    style: 불필요한 주석 삭제

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,6 @@ import Profile from './pages/Profile.js';
 import NotFound from './pages/NotFound.js';
 
 export default class Router {
-  // static 필드로 routes를 설정
   static routes = {
     '/': Home,
     '/login': Login,
@@ -12,24 +11,21 @@ export default class Router {
     '/error': NotFound,
   };
 
-  // static 메서드로 페이지 로드 처리
   static loadPage(path) {
     const Page = this.routes[path] || this.routes['/error'];
     const page = new Page();
     document.getElementById('root').innerHTML = page.render();
 
     if (page.init) {
-      page.init(); // 각 페이지의 init 함수 호출
+      page.init();
     }
   }
 
-  // static 메서드로 경로 변경 처리
   static navigate(path) {
     window.history.pushState({}, '', path);
     this.loadPage(path);
   }
 
-  // static 메서드로 라우터 초기화
   static initRouter() {
     document.addEventListener('click', (e) => {
       if (!e.target.matches('a')) return;

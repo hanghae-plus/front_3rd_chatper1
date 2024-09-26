d74be9e9dd3a436bfe9cce761bb09ba102dc9a44
chyerin <yerin@easytask.co.kr>
2024-09-27 07:29:54 +0900

    feat: 로그아웃 기능 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,40 @@
 import Component from '../base/Component';
 import Router from '../base/Router';
+import userStore from '../store/user';
 
 class Header extends Component {
 	constructor(element) {
 		super(element);
+		userStore.subscribe(() => this.render());
+	}
+
+	mounted() {
+		// 모든 링크에 대해 클릭 이벤트를 바인딩
+		const links = this.$target.querySelectorAll('nav a');
+		links.forEach(this.handleClickLink);
+
+		const logoutBtn = this.$target.querySelector('#logout');
+		if (logoutBtn) {
+			logoutBtn.addEventListener('click', this.handleLogout);
+		}
+	}
+
+	handleClickLink(link) {
+		link.addEventListener('click', (event) => {
+			event.preventDefault();
+			const url = link.getAttribute('href');
+			Router.navigateTo(url); // Router 로 경로 이동
+		});
+	}
+
+	handleLogout() {
+		userStore.logout();
+		Router.navigateTo('/'); // Router 로 경로 이동
 	}
 
 	template() {
+		const isLoggedIn = userStore.getLoginStatus();
+
 		return `
 			<header class="bg-blue-600 text-white p-4 sticky top-0">
 				<h1 class="text-2xl font-bold">항해플러스</h1>
@@ -16,22 +44,10 @@ class Header extends Component {
 				<ul class="flex justify-around">
 					<li><a href="/" class="text-blue-600">홈</a></li>
 					<li><a href="/profile" class="text-gray-600">프로필</a></li>
-					<li><a href="/login" class="text-gray-600">로그인</a></li>
+					<li>${isLoggedIn ? '<button id="logout" class="text-gray-600">로그아웃</button>' : '<a href="/login" class="text-gray-600">로그인</a>'}</li>
 				</ul>
 			</nav>
 		`;
-	}
-
-	mounted() {
-		// 모든 링크에 대해 클릭 이벤트를 바인딩
-		const links = this.$target.querySelectorAll('nav a');
-		links.forEach((link) => {
-			link.addEventListener('click', (event) => {
-				event.preventDefault();
-				const url = link.getAttribute('href');
-				Router.navigateTo(url); // Router 로 경로 이동
-			});
-		});
 	}
 }
 

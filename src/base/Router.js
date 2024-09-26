db931adffe5beb7cc975e541669a35c9042177ac
chyerin802 <chyerin802@gmail.com>
2024-09-26 09:13:13 +0900

    refactor: Router class 문법으로 수정

---
+++ src/base/Router.js
@@ -0,0 +1,33 @@
+class RouterClass {
+	constructor() {
+		this.routes = {};
+	}
+
+	addRoute(path, route) {
+		this.routes[path] = route;
+	}
+
+	init() {
+		window.addEventListener('popstate', () => {
+			this.loadRoute(window.location.pathname);
+		});
+	}
+
+	loadRoute(path) {
+		const route = this.routes[path] || this.routes['*']; // 404 페이지 처리
+		if (route && route.render) {
+			route.render();
+		} else {
+			console.error('Route not found');
+		}
+	}
+
+	navigateTo(url) {
+		window.history.pushState({}, '', url);
+		this.loadRoute(url);
+	}
+}
+
+// Router 인스턴스 생성
+const Router = new RouterClass();
+export default Router;

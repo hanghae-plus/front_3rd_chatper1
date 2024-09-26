f5747fac7204d83b0cb90cafa942c8f66c3b4c36
jhkim <jhkim@enterprise1.co.kr>
2024-09-26 14:54:50 +0900

    [feat] main , router

---
+++ src/components/Profile.js
@@ -0,0 +1,3 @@
+export default function Profile() {
+  alert("Profile입니다.");
+}
---
+++ src/router/Router.js
@@ -0,0 +1,44 @@
+import Error from "../components/Error";
+import Login from "../components/Login";
+import Main from "../components/Main";
+import Profile from "../components/Profile";
+
+class Router {
+  constructor() {
+    this.routes = {};
+    window.addEventListener("popstate", this.handlePopState.bind(this));
+  }
+
+  addRoute(path, handler) {
+    this.routes[path] = handler;
+  }
+
+  navigateTo(path) {
+    history.pushState(null, "", path);
+    this.handlePopState(path);
+  }
+
+  handlePopState() {
+    this.handleRoute(window.location.pathname);
+  }
+
+  handleRoute(path) {
+    const handler = this.routes[path];
+    console.log("handler", handler);
+    if (handler) {
+      handler();
+    } else {
+      router.navigateTo("/error");
+    }
+  }
+}
+
+export const router = new Router();
+
+const pathInfo = [
+  { title: "홈", path: "/", component: Main() },
+  // { title: "로그인", path: "/login", component: Login() },
+  // { title: "프로필", path: "/profile", component: Profile() },
+];
+
+router.addRoute("/", Main());

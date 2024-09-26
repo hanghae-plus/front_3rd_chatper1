29d9e804e6af3c5d03fa32910c2e5595ea662235
chyerin <yerin@easytask.co.kr>
2024-09-27 05:23:13 +0900

    feat: 전역 상태 관리 Store class 구현

---
+++ src/base/Store.js
@@ -0,0 +1,30 @@
+// Observer 패턴을 이용한 Store
+class Store {
+	constructor() {
+		this.state = {}; // 전역 상태
+		this.listeners = []; // 상태 변경을 구독하는 컴포넌트들
+	}
+
+	// 전역 상태 설정
+	setState(newState) {
+		this.state = { ...this.state, ...newState };
+		this.notify(); // 상태 변경 시 모든 리스너에게 알림
+	}
+
+	// 상태 변경 구독
+	subscribe(listener) {
+		this.listeners.push(listener);
+	}
+
+	// 상태 변경 시 모든 구독자에게 알림
+	notify() {
+		this.listeners.forEach((listener) => listener());
+	}
+
+	// 전역 상태 가져오기
+	getState() {
+		return this.state;
+	}
+}
+
+export default Store;

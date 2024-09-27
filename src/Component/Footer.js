2367528175c9933b11f3d372c452fc12e299f1df
Ensil <lasags@naver.com>
2024-09-27 09:25:29 +0900

    리팩토링) 주석정리

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,5 +1,3 @@
-//? 미구현: Page 컴포넌트에 적절한 타이밍에 붙여서 함께 사용하는 방법에 대한 고민중
-
 export const Footer = () => {
     return `
       <footer class="bg-gray-200 p-4 text-center">
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,3 @@
-//? 미구현: Page 컴포넌트에 적절한 타이밍에 붙여서 함께 사용하는 방법에 대한 고민중
-
 export const Header = () => {
     return `
   <header class="bg-blue-600 text-white p-4 sticky top-0">

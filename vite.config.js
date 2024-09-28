2d2ffb6c781ee4d6b8e904f10206babe866389cb
junilhwang <junil.h@kakao.com>
2024-09-28 13:19:07 +0900

    feat: chapter1-2 초기설정 완료

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
-  },
-})
+export default mergeConfig(
+  defineConfig({
+    esbuild: {
+      jsxFactory: 'createVNode',
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        jsx: 'transform',
+        jsxFactory: 'createVNode',
+      }
+    }
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.js'
+    },
+  })
+);

9e5ca5e90fdcc7edf73a455e148f0878666af397
soyoonJ <sytkfkd96@gmail.com>
2024-09-28 22:27:26 +0900

    feat: readme -> jsx 컴포넌트 내용 옮기기

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,28 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
 
-export const Navigation = () => ({});
+const getNavItemClass = (path) => {
+	const currentPath = window.location.pathname;
+	return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+};
+
+export const Navigation = ({ loggedIn }) => (
+	<nav class="bg-white shadow-md p-2 sticky top-14">
+		<ul class="flex justify-around">
+			<li>
+				<a href="/" class="${getNavItemClass('/')}" data-link>
+					홈
+				</a>
+			</li>
+			$
+			{!loggedIn
+				? `<li><a href="/login" class="${getNavItemClass("/login")}" data-link>로그인</a></li>`
+				: ""}
+			$
+			{loggedIn
+				? `<li><a href="/profile" class="${getNavItemClass("/profile")}" data-link>프로필</a></li>`
+				: ""}
+			${loggedIn ? `<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>` : ""}
+		</ul>
+	</nav>
+);

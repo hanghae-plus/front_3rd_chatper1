46156d198bdccd7d6d395cd5750c7705b14cf4bd
HaEun <rjadmszhd777@naver.com>
2024-09-29 02:54:36 +0900

    fix: HomePage 주석지우기

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,21 +8,15 @@ import { Post } from '../components';
 export const HomePage = () => {
     const { loggedIn, posts } = globalStore.getState();
     return (
-        // <div className="bg-gray-100 min-h-screen flex justify-center">
-        <div className="max-w-md w-full">
-
+        <div className="bg-gray-100 min-h-screen flex justify-center">
             <div className="max-w-md w-full">
                 <Header />
-                {loggedIn ? NotFoundPage() : ""}
                 <Navigation loggedIn={loggedIn} />
                 
                 <main className="p-4">
                     {loggedIn && <NotFoundPage />}
                     <div id="posts-container" className="space-y-4">
                         {posts.map(Post)}
-                        {/* {posts.map((post, index) => (
-                            <Post key={index} post={post} />
-                        ))} */}
                     </div>
                 </main>
                 

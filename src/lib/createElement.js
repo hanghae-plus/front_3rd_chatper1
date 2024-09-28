ecd5c1b5d24b0d917c25f17521dded67be23cfd1
JANG DONG JIN <jdwls27@naver.com>
2024-09-29 01:36:22 +0900

    createElement 문자열 입력에 대해 텍스트 노드 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -7,8 +7,12 @@
 //    - vNode.type에 해당하는 요소를 생성
 //    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
-
 export function createElement(vNode) {
-  // 여기에 구현하세요
+
+  if(typeof vNode === 'string'){
+    return document.createTextNode(vNode);
+  }
+
+
   return {}
 }

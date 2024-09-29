a025333809b05e83acf734eb17e1810de5c62a9b
eggmun98 <eggmun98@gmail.com>
2024-09-29 22:14:21 +0900

    feat: children falsy값 필터처리 로직 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -9,6 +9,6 @@ export function createVNode(type, props, ...children) {
   return {
     type,
     props,
-    children: children.flat(Infinity),
+    children: children.flat(Infinity).filter((el) => el),
   };
 }

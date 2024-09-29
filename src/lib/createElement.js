f789190f7818893de25823fab2155a920ee99f46
eggmun98 <eggmun98@gmail.com>
2024-09-29 22:04:26 +0900

    feat: vNode가 falsy 경우와 vNode가 문자열 또는 숫자형일 경우 분기 처리 로직 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,4 +1,5 @@
 // TODO: createElement 함수 구현
+import { createVNode } from "./createVNode";
 // 1. vNode가 falsy면 빈 텍스트 노드를 반환합니다.
 // 2. vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
 // 3. vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
@@ -9,6 +10,26 @@
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
 
 export function createElement(vNode) {
-  // 여기에 구현하세요
-  return {}
+  // 1
+  if (!vNode) {
+    return document.createTextNode("");
+  }
+
+  // 2
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  // if (Array.isArray(vNode)) {
+  //   const len = vNode.length;
+
+  //   if (len) {
+
+  //   } else {
+  //   }
+  // }
+
+  // 5
+  createVNode({ ...vNode });
+  return {};
 }

d5681171a083f2fc9d9f06a615841bbd39077e3c
yujinkim11 <106130311+yujinkim11@users.noreply.github.com>
2024-09-29 20:04:38 +0900

    feat: createVNode 함수 작성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -5,6 +5,7 @@
 // 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
 
 export function createVNode(type, props, ...children) {
-  // 여기에 구현하세요
-  return {}
+  // 평탄화 및 falsy 값 필터링
+  const flatChildren = children.flat(Infinity).filter(Boolean);
+  return { type, props, children: flatChildren };
 }

e6b69f2280464d8b33e2f35688a7d38900c61a36
Jack <relee6203@gmail.com>
2024-09-29 19:58:42 +0900

    refactor: Infinity를 이용한 createVNode 로직 수적

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -4,27 +4,6 @@
 // 3. children은 평탄화(flat)되어야 하며, falsy 값은 필터링되어야 합니다.
 // 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
 
-import { flattenChildren } from '../utils/arrayUtils';
-
-export const flattenChildren = (children) => {
-  return children.flat().filter(Boolean);
-};
-
 export function createVNode(type, props, ...children) {
-  // 여기에 구현하세요
-  let _children = [];
-
-  for (const child of children) {
-    if (!child) continue;
-    if (typeof child === 'string') {
-      _children.push(child);
-    } else if (Array.isArray(child)) {
-      _children.push(...flattenChildren(child));
-      //TODO: createVNode로 생성된 객체인지 판별하는 식 추가
-    } else {
-      _children.push(child);
-    }
-  }
-
-  return { type, props, children: _children };
+  return { type, props, children: children.flat(Infinity).filter(Boolean) };
 }

459579c091cb1948cc9559cc34af37599a7c7647
ooyuo <doyounjeong0@gmail.com>
2024-09-29 20:58:56 +0900

    feat: page, component jsx 형식으로 변경

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,30 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
 
-export const Navigation = () => ({});
+export const Navigation = ({ loggedIn }) => (
+  <nav class="bg-white shadow-md p-2 sticky top-14">
+    <ul class="flex justify-around">
+      <li>
+        <a href="/" class="${getNavItemClass('/')}" data-link>
+          홈
+        </a>
+      </li>
+      $
+      {!loggedIn
+        ? `<li><a href="/login" class="${getNavItemClass(
+            "/login"
+          )}" data-link>로그인</a></li>`
+        : ""}
+      $
+      {loggedIn
+        ? `<li><a href="/profile" class="${getNavItemClass(
+            "/profile"
+          )}" data-link>프로필</a></li>`
+        : ""}
+      $
+      {loggedIn
+        ? `<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>`
+        : ""}
+    </ul>
+  </nav>
+);

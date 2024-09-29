98074f3ffb6066585a2f172b13107e2e7fb4b7e4
JANG DONG JIN <jdwls27@naver.com>
2024-09-29 23:56:02 +0900

    createVNode 간소화

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -6,6 +6,6 @@
 
 export function createVNode(type, props, ...children) {
 
-    return { type, props, children: children.flat(Infinity).filter(child => Boolean(child)) }
+    return { type, props, children: children.flat(Infinity).filter(Boolean) }
 
 }

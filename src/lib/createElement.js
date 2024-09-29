6edb8934597deb103c645af2da9b9e646b006337
Jack <relee6203@gmail.com>
2024-09-29 16:42:50 +0900

    feat: createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -9,6 +9,38 @@
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
 
 export function createElement(vNode) {
-  // 여기에 구현하세요
-  return {}
+  if (!vNode) return document.createTextNode('');
+
+  if (typeof vNode === 'string' || typeof vNode === 'number') return document.createTextNode(vNode);
+  if (Array.isArray(vNode)) {
+    const fragment = new DocumentFragment();
+    vNode.forEach((child) => fragment.appendChild(createElement(child)));
+    return fragment;
+  }
+
+  if (typeof vNode.type === 'function') {
+    return createElement(vNode.type(vNode.props));
+  }
+
+  const element = document.createElement(vNode.type);
+
+  for (const key in vNode.props) {
+    const value = vNode.props[key];
+
+    if (key.startsWith('data-')) {
+      element.dataset[key.slice(5)] = value;
+    } else if (key === 'className') {
+      element.className = value;
+    } else if (key.startsWith('on') && typeof value === 'function') {
+      element.addEventListener(key.slice(2), value());
+    } else {
+      element[key] = value;
+    }
+  }
+
+  for (const child of vNode.children) {
+    element.appendChild(createElement(child));
+  }
+
+  return element;
 }

4d94642eda6386f692b66c2a53a9c34074a46f2c
terry.hwang <terry.hwang@h2ohospitality.io>
2024-09-29 23:07:21 +0900

    feat : Footer컴포넌트 추가

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,6 +1,9 @@
-/** @jsx createVNode */
-import { createVNode } from "../../lib";
+import { createVNode } from "@/lib";
 
 export function Footer() {
-  return {};
+  return createVNode(
+    "footer",
+    { class: "bg-gray-200 p-4 text-center" },
+    createVNode("p", null, "© 2024 항해플러스. All rights reserved.")
+  );
 }

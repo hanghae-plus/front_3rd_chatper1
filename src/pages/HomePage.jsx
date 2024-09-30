064d55db897a18c8c046a1d02cca606ccb1827fc
taeyoung <terry.hwang@h2ohospitality.io>
2024-09-30 14:40:28 +0900

    feat : HomePage 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
 /** @jsx createVNode */
 import { createVNode } from "../lib";
 
+import { Post, PostForm } from "@/components";
+
+import { globalStore } from "@/stores/globalStore";
+
 export function HomePage() {
-  return {};
+  const { getState } = globalStore;
+  const { loggedIn, posts } = getState();
+
+  return (
+    <div>
+      {loggedIn && <PostForm />}
+      <div className="space-y-4">
+        {posts.map((post) => (
+          <Post key={post.id} {...post} />
+        ))}
+      </div>
+    </div>
+  );
 }

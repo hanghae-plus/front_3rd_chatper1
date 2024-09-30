7ce7f4aa2565d521e13c7d30bc71841fa74678e8
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-30 17:28:03 +0900

    ✨ feat(HomePage) :: HomePage 컴포넌트 jsx로 정의

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,25 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { createVNode } from "../lib";
+import { Navigation } from "../components/templates/Navigation";
+import { Footer, Header, Post } from "../components";
+import { globalStore } from "../stores";
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { postsItems } = globalStore.getState();
+  return (
+    <div>
+      <div className="max-w-md w-full">
+        <Header />
+        <Navigation />
+        <main className="p-4">
+          <div id="posts-container" className="space-y-4">
+            {postsItems.map(({ author, time, content, id }) => (
+              <Post postId={id} author={author} time={time} content={content} />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};
--- src/stores/globalStore.js
+++ src/stores/globalStore.js
@@ -4,7 +4,7 @@ import { userStorage } from "../storages";
 export const globalStore = createStore({
   currentUser: userStorage.get('user'),
   loggedIn: Boolean(userStorage.get('user')),
-  posts: [
+  postsItems: [
     { id: 1, author: '홍길동', time: '5분 전', content: '오늘 날씨가 정말 좋네요. 다들 좋은 하루 보내세요!' },
     { id: 2, author: '김철수', time: '15분 전', content: '새로운 프로젝트를 시작했어요. 열심히 코딩 중입니다!' },
     { id: 3, author: '이영희', time: '30분 전', content: '오늘 점심 메뉴 추천 받습니다. 뭐가 좋을까요?' },

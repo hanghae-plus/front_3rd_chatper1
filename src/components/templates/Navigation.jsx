ec52320b362b045d9cf48d800c36d3ec9ae3b321
hyunjin.noh <hyunjin.noh@saltlux.com>
2024-10-01 05:34:58 +0900

    [feat] 공통 컴포넌트 구현
     - footer
     - header
     - navigation

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,51 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from '../../lib';
 
-export const Navigation = () => ({});
+export const Navigation = ({ loggedIn }) => {
+  const currentPath = window.location.pathname;
+
+  const getLinkClass = (path) => {
+    return currentPath === path ? 'text-blue-600 font-bold' : 'text-gray-600';
+  };
+
+  return (
+    <nav className='bg-white shadow-md p-2 sticky top-14'>
+      <ul className='flex justify-around'>
+        <li>
+          <a href='/' className={getLinkClass('/')} data-link='true'>
+            홈
+          </a>
+        </li>
+        {!loggedIn && (
+          <li>
+            <a
+              href='/login'
+              className={getLinkClass('/login')}
+              data-link='true'
+            >
+              로그인
+            </a>
+          </li>
+        )}
+        {loggedIn && (
+          <li>
+            <a
+              href='/profile'
+              className={getLinkClass('/profile')}
+              data-link='true'
+            >
+              프로필
+            </a>
+          </li>
+        )}
+        {loggedIn && (
+          <li>
+            <a href='#' id='logout' className='text-gray-600'>
+              로그아웃
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};

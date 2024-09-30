d2ec46927bc495e99fe172cfc9e27d4ea32ed351
Ben(석금용) <gmyong21@gmail.com>
2024-09-30 17:11:27 +0900

    feat: jsx 파일 템플릿 초기 세팅

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,4 +1,10 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from '../../lib';
 
-export const Footer = () => ({});
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-200 p-4 text-center">
+      <p>&copy; 2024 항해플러스. All rights reserved.</p>
+    </footer>
+  );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,29 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { createVNode } from '../lib';
+import { globalStore } from '../stores/index.js';
+import { Footer, Header, Navigation, Post, PostForm } from '../components/index.js';
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+
+  return (
+    <div className="bg-gray-100 min-h-screen flex justify-center">
+      <div className="max-w-md w-full">
+        <Header />
+
+        <Navigation loggedIn={loggedIn} />
+
+        <main className="p-4">
+          {loggedIn && <PostForm />}
+          <div id="posts-container" className="space-y-4">
+            {posts.map(({ id, author, time, content }) => {
+              return <Post id={id} author={author} time={time} content={content} />;
+            })}
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  );
+};

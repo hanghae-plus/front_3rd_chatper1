ffed5ccf5a00efa87901baa9f398973955256861
soyoonJ <sytkfkd96@gmail.com>
2024-10-01 00:43:47 +0900

    feat: Navigation.jsx logout, navigateTo 기능 추가

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,14 +1,30 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { router } from "../../main";
+import { userStorage } from "../../storages";
+import { globalStore } from "../../stores";
 
 const getNavItemClass = (path) => {
 	const currentPath = window.location.pathname;
 	return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
 };
+const navigateTo = (e) => {
+	e.preventDefault();
+	router.push(e.target.href.replace(window.location.origin, ""));
+};
+function logout() {
+	globalStore.setState({ currentUser: null, loggedIn: false });
+	router.push("/login");
+	userStorage.reset();
+}
+const handleLogout = (e) => {
+	e.preventDefault();
+	logout();
+};
 
 export const Navigation = ({ loggedIn }) => (
 	<nav className="bg-white shadow-md p-2 sticky top-14">
-		<ul className="flex justify-around">
+		<ul className="flex justify-around" onClick={navigateTo}>
 			<li>
 				<a href="/" className={getNavItemClass("/")} data-link>
 					홈
@@ -36,7 +52,7 @@ export const Navigation = ({ loggedIn }) => (
 			)}
 			{loggedIn ? (
 				<li>
-					<a href="#" id="logout" className="text-gray-600">
+					<a href="#" onClick={handleLogout} id="logout" className="text-gray-600">
 						로그아웃
 					</a>
 				</li>

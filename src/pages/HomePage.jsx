5ff17a0bc584bc81dae178bc3b2316b48d42a27d
김진현 <jinhyeon0810@gmail.com>
2024-09-30 23:29:47 +0900

    feat HomePage,LoginPage,NotFoundPage,ProfilePage 구현

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,29 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { globalStore } from "../stores";
+import { createVNode } from "../lib";
+import { Header } from "../components/templates/Header";
+import { Footer } from "../components/templates/Footer";
+import { Navigation } from "../components/templates/Navigation";
+import { PostForm } from "../components/posts/PostForm";
+import { Post } from "../components/posts/Post";
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+  return (
+    <div class="bg-gray-100 min-h-screen flex justify-center">
+      <div className="max-w-md w-full">
+        <Header />
+        <Navigation />
+        <main className="p-4">
+          {loggedIn && <PostForm />}
+          <div id="posts-container" className="space-y-4">
+            {posts.map(({ id, author, time, content }) => (
+              <Post postId={id} author={author} time={time} content={content} />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};

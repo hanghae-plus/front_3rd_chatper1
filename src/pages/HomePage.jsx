dae1dd85825e4b4fc307b378a1b4bb4afff324a0
남균 <gyuni9604@naver.com>
2024-10-01 00:14:24 +0900

    ✨ feat : 홈페이지 jsx 파일 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,27 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { Footer, Post } from "../components"
+import { Header } from "../components/templates/Header"
+import { Navigation } from "../components/templates/Navigation"
+import { createVNode } from "../lib"
+import { globalStore } from "../stores"
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { posts } = globalStore.getState()
+
+  return (
+    <div>
+      <div className="max-w-md w-full">
+        <Header />
+        <Navigation />
+        <main className="p-4">
+          <div id="posts-container" class="space-y-4">
+            {posts.map(({ author, time, content, id }) => (
+              <Post author={author} time={time} content={content} id={id} />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  )
+}

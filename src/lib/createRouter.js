e8ac6363282f2338d190bc5c3384376dcda22801
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-30 14:37:13 +0900

    ✨ feat(createRouter) :: 일반 페이지 및 NotFound 라우팅 구현

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -3,12 +3,15 @@ import { createObserver } from "./createObserver";
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getTarget = () => routes[window.location.pathname]
+  const getTarget = () =>
+    routes[window.location.pathname]
+      ? routes[window.location.pathname]
+      : routes['/404']
 
   const push = (path) => {
     window.history.pushState(null, null, path);
     notify();
-  }
+  };
 
   window.addEventListener('popstate', () => notify());
 

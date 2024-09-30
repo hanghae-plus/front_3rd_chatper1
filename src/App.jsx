7035df808548727a2c17ff629a0c1ec47f33a634
taeyoung <terry.hwang@h2ohospitality.io>
2024-09-30 14:54:01 +0900

    feat : RootLayout 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 /** @jsx createVNode */
 import { createVNode } from "./lib";
 
-export function App() {
-  return {};
+import { RootLayout } from "@/components";
+
+export function App(props) {
+  const { targetPage } = props || {};
+  return <RootLayout>{targetPage}</RootLayout>;
 }

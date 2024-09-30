50de38170d014e92164957da31996d82838b0a42
taeyoung <terry.hwang@h2ohospitality.io>
2024-09-30 14:12:25 +0900

    feat : PostForm 컴포넌트 추가

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -2,5 +2,15 @@
 import { createVNode } from "../../lib";
 
 export function PostForm() {
-  return {};
+  return (
+    <form className="mb-4 bg-white rounded-lg shadow p-4">
+      <textarea
+        className="w-full p-2 border rounded"
+        placeholder="무슨 생각을 하고 계신가요?"
+      ></textarea>
+      <button className="mt-2 bg-blue-600 text-white px-4 py-2 rounded">
+        게시
+      </button>
+    </form>
+  );
 }

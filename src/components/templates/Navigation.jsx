75c00e3d885f44c92fcf09dc8674c38229497bdf
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-01 00:42:06 +0900

    fix: 로그인 상태에 따른 UI변경 처리

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,21 +1,42 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
 
-export const Navigation = () => {
+const getNavItemClass = (path) => {
+    const currentPath = window.location.pathname;
+    return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+};
+
+export const Navigation = ({ loggedIn }) => {
     return (
         <nav className="bg-white shadow-md p-2 sticky top-14">
             <ul className="flex justify-around">
                 <li>
-                    <a href="/" className="text-blue-600 font-bold" data-link="true">
+                    <a href="/" className={getNavItemClass("/")} data-link="true">
                         홈
                     </a>
                 </li>
-                <li>
-                    <a href="/login" className="text-gray-600" data-link="true">
-                        로그인
-                    </a>
-                    {/* ${!loggedIn ? `<li><a href="/login" class="${getNavItemClass("/login")}" data-link>로그인</a></li>` : ""}${loggedIn ? `<li><a href="/profile" class="${getNavItemClass("/profile")}" data-link>프로필</a></li>` : ""}${loggedIn ? `<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>` : ""} */}
-                </li>
+                {!loggedIn && (
+                    <li>
+                        <a href="/login" className={getNavItemClass("/login")} data-link="true">
+                            로그인
+                        </a>
+                    </li>
+                )}
+                {loggedIn && (
+                    <li>
+                        <a href="/profile" className={getNavItemClass("/profile")} data-link="true">
+                            프로필
+                        </a>
+                    </li>
+                )}
+
+                {loggedIn && (
+                    <li>
+                        <a href="#" id="logout" className={getNavItemClass("/login")}>
+                            로그아웃
+                        </a>
+                    </li>
+                )}
             </ul>
         </nav>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,19 @@
 /** @jsx createVNode */
 import { createVNode } from "../lib";
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
+import { globalStore } from "../stores";
 
 export const HomePage = () => {
+    const { loggedIn, currentUser, posts } = globalStore.getState();
     return (
         <div class="bg-gray-100 min-h-screen flex justify-center">
             <div class="max-w-md w-full">
                 {Header()}
-                {Navigation()}
+                {Navigation({ loggedIn })}
                 <main class="p-4">
-                    {PostForm()}
+                    {loggedIn && PostForm()}
                     <div id="posts-container" class="space-y-4">
-                        {/* {posts.map(Post)} */}
+                        {posts.map(Post)}
                     </div>
                 </main>
                 {Footer()}

7ada1e8ceb12e6c092299809b463f127eef421b8
이동아 <christar99@naver.com>
2024-09-30 23:40:31 +0900

    jsx컴포넌트 템플릿 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 /** @jsx createVNode */
 import { createVNode } from '../lib';
 
-import { Footer, Header, Navigation } from '../components';
+import { Footer, Header, Navigation, Post } from '../components';
 import { NotFoundPage } from './NotFoundPage';
 import { globalStore } from '../stores';
 
@@ -11,13 +11,18 @@ export const HomePage = () => {
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
         <Header />
+
         <Navigation loggedIn={loggedIn} />
+
         <main class="p-4">
           {loggedIn ? <NotFoundPage /> : ''}
           <div id="posts-container" class="space-y-4">
-            {/* {posts.map(ProfilePage).join('')} */}
+            {posts.map(props => (
+              <Post {...props} />
+            ))}
           </div>
         </main>
+
         <Footer />
       </div>
     </div>

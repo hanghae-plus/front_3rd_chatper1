c330fd1c0d470512ca9a57947b1aaf0d7f5f89a7
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-30 14:32:48 +0900

    ✨ feat(Navigation) :: 헤더 Navigation 컴포넌트 jsx로 정의

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,52 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
+import { userStorage } from "../../storages";
 
-export const Navigation = () => ({});
+export const Navigation = () => {
+  const loggedIn = userStorage.get("user");
+
+  const getNavigateList = (href) => {
+    return location.pathname == href
+      ? "text-blue-600 font-bold"
+      : "text-gray-600";
+  };
+
+  return (
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+        <li>
+          <a href="/" class={getNavigateList("/")} data-link>
+            홈
+          </a>
+        </li>
+        {loggedIn ? (
+          <li>
+            <a href="/profile" class={getNavigateList("/profile")} data-link>
+              프로필
+            </a>
+          </li>
+        ) : (
+          ''
+        )}
+        {!loggedIn ? (
+          <li>
+            <a href="/login" class={getNavigateList("/login")} data-link>
+              로그인
+            </a>
+          </li>
+        ) : (
+          ''
+        )}
+        {loggedIn ? (
+          <li>
+            <a href="#" id="logout" class="text-gray-600">
+              로그아웃
+            </a>
+          </li>
+        ) : (
+          ''
+        )}
+      </ul>
+    </nav>
+  );
+};

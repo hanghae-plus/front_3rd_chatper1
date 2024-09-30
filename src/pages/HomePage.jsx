d8142dcc829c2a5070b12b28b8aea3cb9a8456e4
dongjang <jdwls27@naver.com>
2024-09-30 13:43:28 +0900

    homePage 재정의

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,30 +2,34 @@
 import{ createVNode } from "../lib";
 import { globalStore } from '../stores';
 import { Navigation } from '../components/templates/Navigation';
-import { PostForm } from "../components";
+import { Footer, Header, PostForm } from "../components";
 import { Post } from "../components";
 
-export const HomePage = () => { 
+
+export const HomePage = ({isComponentShow}) => { 
   const { loggedIn, posts } = globalStore.getState(); 
   return (            
-      <div className="bg-gray-100 min-h-screen flex justify-center"> 
         <div className="max-w-md w-full"> 
-          {<Navigation loggedIn={loggedIn}/>} 
-          <main className="p-4"> 
-          {loggedIn ? <PostForm loggedIn={loggedIn}/>:''} 
+
+            {isComponentShow ? <Header />: ''} 
+            {<Navigation loggedIn={loggedIn}/>} 
+
+            <main className="p-4"> 
+                {loggedIn ? <PostForm loggedIn={loggedIn}/>:''} 
+                <div id="posts-container" className="space-y-4"> 
+                    {posts.map(post => (
+                        <Post 
+                            key={post.id} 
+                            author={post.author} 
+                            time={post.time} 
+                            content={post.content} 
+                            id={post.id} 
+                        />
+                    ))}
+                </div> 
           </main> 
-          <div id="posts-container" className="space-y-4"> 
-            {posts.map(post => (
-                <Post 
-                    key={post.id} 
-                    author={post.author} 
-                    time={post.time} 
-                    content={post.content} 
-                    id={post.id} 
-                />
-            ))}
-          </div> 
+
+          {isComponentShow ? <Footer/>: ''} 
         </div> 
-      </div> 
   ); 
 };

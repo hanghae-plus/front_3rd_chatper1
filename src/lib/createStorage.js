0d53f1f8305ff18768dc33b5f441d00898c76619
김진현 <jinhyeon0810@gmail.com>
2024-09-30 23:30:56 +0900

    feat createStore 시 key값을 함수 밖에서 핸들링(확장성 고려)

--- src/lib/createStorage.js
+++ src/lib/createStorage.js
@@ -1,7 +1,7 @@
-export const createStorage = (key, storage = window.localStorage) => {
-  const get = () => JSON.parse(storage.getItem(key));
-  const set = (value) => storage.setItem(key, JSON.stringify(value));
-  const reset = () => storage.removeItem(key);
+export const createStorage = (storage = window.localStorage) => {
+  const get = (key) => JSON.parse(storage.getItem(key));
+  const set = (key, value) => storage.setItem(key, JSON.stringify(value));
+  const reset = (key) => storage.removeItem(key);
 
   return { get, set, reset };
-}
+};

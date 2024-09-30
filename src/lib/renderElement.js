f1317e36bb957e63ffc8460b1b74a3e1c8e433c0
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-30 18:59:48 +0900

    ✨ feat ::  렌더링 요소 함수 renderElement 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,65 +1,67 @@
-// renderElement.js
-import { addEvent, removeEvent, setupEventListeners } from './eventManager';
 import { createElement__v2 } from "./createElement__v2.js";
+import { setupEventListeners } from './eventManager';
 
-// TODO: processVNode 함수 구현
-function processVNode() {
-  // vNode를 처리하여 렌더링 가능한 형태로 변환합니다.
-  // - null, undefined, boolean 값 처리
-  // - 문자열과 숫자를 문자열로 변환
-  // - 함수형 컴포넌트 처리 <---- 이게 제일 중요합니다.
-  // - 자식 요소들에 대해 재귀적으로 processVNode 호출
+function processVNode(vNode) {
+    if (!vNode) return document.createTextNode('');
+    if (typeof vNode === 'string' || typeof vNode === 'number') {
+        return document.createTextNode(String(vNode));
+    }
+    if (Array.isArray(vNode)) {
+        const fragment = document.createDocumentFragment();
+        vNode.forEach(child => fragment.appendChild(processVNode(child)));
+        return fragment;
+    }
+    const element = document.createElement(vNode.type);
+    applyProps(element, vNode.props);
+    vNode.children.map(child => processVNode(child)).forEach(childElement => element.appendChild(childElement));
+    return element;
 }
 
-// TODO: updateAttributes 함수 구현
-function updateAttributes() {
-  // DOM 요소의 속성을 업데이트합니다.
-  // - 이전 props에서 제거된 속성 처리
-  // - 새로운 props의 속성 추가 또는 업데이트
-  // - 이벤트 리스너, className, style 등 특별한 경우 처리
-  //   <이벤트 리스너 처리>
-  //     - TODO: 'on'으로 시작하는 속성을 이벤트 리스너로 처리
-  //     - 주의: 직접 addEventListener를 사용하지 않고, eventManager의 addEvent와 removeEvent 함수를 사용하세요.
-  //     - 이는 이벤트 위임을 통해 효율적으로 이벤트를 관리하기 위함입니다.
+function applyProps(element, props, oldProps = {}) {
+    Object.keys({...props, ...oldProps}).forEach(key => {
+        const newVal = props[key];
+        const oldVal = oldProps[key];
+        if (newVal !== oldVal) {
+            if (key.startsWith('on')) {
+                const eventType = key.slice(2).toLowerCase();
+                if (oldVal) element.removeEventListener(eventType, oldVal);
+                if (newVal) element.addEventListener(eventType, newVal);
+            } else if (newVal == null) {
+                element.removeAttribute(key);
+            } else {
+                element.setAttribute(key, newVal);
+            }
+        }
+    });
 }
 
-// TODO: updateElement 함수 구현
-function updateElement() {
-  // 1. 노드 제거 (newNode가 없고 oldNode가 있는 경우)
-  // TODO: oldNode만 존재하는 경우, 해당 노드를 DOM에서 제거
-
-  // 2. 새 노드 추가 (newNode가 있고 oldNode가 없는 경우)
-  // TODO: newNode만 존재하는 경우, 새 노드를 생성하여 DOM에 추가
-
-  // 3. 텍스트 노드 업데이트
-  // TODO: newNode와 oldNode가 둘 다 문자열 또는 숫자인 경우
-  // TODO: 내용이 다르면 텍스트 노드 업데이트
-
-  // 4. 노드 교체 (newNode와 oldNode의 타입이 다른 경우)
-  // TODO: 타입이 다른 경우, 이전 노드를 제거하고 새 노드로 교체
-
-  // 5. 같은 타입의 노드 업데이트
-  // 5-1. 속성 업데이트
-  // TODO: updateAttributes 함수를 호출하여 속성 업데이트
-
-  // 5-2. 자식 노드 재귀적 업데이트
-  // TODO: newNode와 oldNode의 자식 노드들을 비교하며 재귀적으로 updateElement 호출
-  // HINT: 최대 자식 수를 기준으로 루프를 돌며 업데이트
-
-  // 5-3. 불필요한 자식 노드 제거
-  // TODO: oldNode의 자식 수가 더 많은 경우, 남은 자식 노드들을 제거
+function updateElement(parent, newNode, oldNode = {}, index = 0) {
+    if (!oldNode && newNode) {
+        parent.appendChild(createElement__v2(newNode));
+    } else if (!newNode) {
+        if (parent.childNodes[index]) parent.removeChild(parent.childNodes[index]);
+    } else if (changed(newNode, oldNode)) {
+        parent.replaceChild(createElement__v2(newNode), parent.childNodes[index]);
+    } else if (newNode.type) {
+        updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
+        const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+        for (let i = 0; i < maxLength; i++) {
+            updateElement(parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+        }
+        while (parent.childNodes[index].childNodes.length > maxLength) {
+            parent.childNodes[index].removeChild(parent.childNodes[index].lastChild);
+        }
+    }
 }
 
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 ||
+        typeof node1 === 'string' && node1 !== node2 ||
+        node1.type !== node2.type;
+}
 
 export function renderElement(vNode, container) {
-  const existing = container.firstChild;
-  const newElement = createElement__v2(vNode);
-
-  if (existing) {
-      container.replaceChild(newElement, existing);
-  } else {
-      container.appendChild(newElement);
-  }
-
-  setupEventListeners(container);
+    updateElement(container, vNode, container.firstChild ? container.firstChild.vNode : null);
+    container.vNode = vNode; 
+    setupEventListeners(container);
 }

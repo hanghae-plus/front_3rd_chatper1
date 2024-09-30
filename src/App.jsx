99dc7ed536f581535b2e420d9f42c98096aea975
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-30 17:28:44 +0900

    ✨ feat(App) :: 최상단 app 컴포넌트 정의

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
 /** @jsx createVNode */
-import{ createVNode } from "./lib";
+import { createVNode } from "./lib";
+import { globalStore } from "./stores";
 
-export const App = () => ({});
+export const App = ({ targetPage }) => {
+  const error = globalStore.getState().error;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
+      {error ? (
+        <p id="error-boundary" class="font-bold p-10 text-red-600" role="alert">
+          오류 발생! ${error.message || '알 수 없는 오류가 발생했습니다.'}
+        </p>
+      ) : (
+        targetPage()
+      )}
+    </div>
+  );
+};

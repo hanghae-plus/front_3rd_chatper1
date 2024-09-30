b1899e2013b70fe8754f6b49d520d5e310b8920a
김진현 <jinhyeon0810@gmail.com>
2024-09-30 23:28:40 +0900

    feat Header,Footer,Post,PostForm,Navigation 컴포넌트 구현

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,41 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
+import { userStorage } from "../../storages";
 
-export const Navigation = () => ({});
+const getStyle = (href) => {
+  const currentPath = window.location.pathname;
+  return currentPath === href ? "text-blue-600 font-bold" : "text-gray-600";
+};
+
+const createLink = (href, name) => {
+  return (
+    <li>
+      <a href={href} className={getStyle(href)} data-link>
+        {name}
+      </a>
+    </li>
+  );
+};
+
+export const Navigation = () => {
+  const loggedIn = userStorage.get("user");
+  return (
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+        {createLink("/", "홈")}
+
+        {loggedIn
+          ? createLink("/profile", "프로필")
+          : createLink("/login", "로그인")}
+
+        {loggedIn && (
+          <li>
+            <a href="#" id="logout" className="text-gray-600">
+              로그아웃
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};

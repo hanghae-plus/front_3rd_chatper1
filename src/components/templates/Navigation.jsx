ec70b534f0aa2fb31b34e30fae4bdb999eab2ca5
남균 <gyuni9604@naver.com>
2024-10-01 00:13:32 +0900

    ✨ feat : template 컴포넌트 추가

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,40 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib"
+import { globalStore } from "../../stores"
 
-export const Navigation = () => ({});
+export const Navigation = () => {
+  const { loggedIn } = globalStore.getState()
+  const currentStyle = (href) => {
+    return location.pathname === href
+      ? "text-blue-600 font-bold"
+      : "text-gray-600"
+  }
+
+  const createLi = (href, text) => {
+    return (
+      <li>
+        <a href={href} class={currentStyle(href)} data-link="true">
+          {text}
+        </a>
+      </li>
+    )
+  }
+
+  return (
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+        {createLi("/", "홈")}
+        {loggedIn
+          ? createLi("/profile", "프로필")
+          : createLi("/login", "로그인")}
+        {loggedIn && (
+          <li>
+            <a href="/login" id="logout" class={currentStyle("/login")}>
+              로그아웃
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}

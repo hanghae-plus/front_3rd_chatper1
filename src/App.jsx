d20832adcb25f6acc63ced5c2b5cab78cc3c1596
dongjang <jdwls27@naver.com>
2024-09-30 13:19:53 +0900

    App.jsx 정의

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 /** @jsx createVNode */
 import{ createVNode } from "./lib";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { globalStore } from "./stores";
+import { Footer, Header } from "./components";
+import { HomePage, ProfilePage } from "./pages";
 
-export const App = () => ({});
+export const App = ({ targetPage }) => {
+    const PageComponent = targetPage;
+    const error = globalStore.getState().error;
+
+    const isComponentShow = targetPage === HomePage || targetPage === ProfilePage;
+
+    return (
+        <div>
+            {isComponentShow ? <Header /> : null}
+            {!error ? <PageComponent /> : <NotFoundPage />}
+            {isComponentShow ? <Footer /> : null}
+        </div>
+    );
+};

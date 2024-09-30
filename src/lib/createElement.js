fe7ecd9c6acd33ff62644e77219e7b45698534ad
이동아 <christar99@naver.com>
2024-09-30 23:40:50 +0900

    createElement 함수 작성. 기본과제 테스트 완료

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -8,7 +8,55 @@
 //    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
 
+const events = ['onClick', 'onChange', 'onInput', 'onSubmit'];
+
 export function createElement(vNode) {
-  // 여기에 구현하세요
-  return {}
+  let $el;
+
+  if (['string', 'number', 'bigint'].includes(typeof vNode)) {
+    $el = document.createTextNode(vNode);
+    return $el;
+  }
+
+  if ([null, false].includes(vNode)) {
+    $el = document.createTextNode('');
+    return $el;
+  }
+
+  if (Array.isArray(vNode)) {
+    $el = new DocumentFragment();
+    vNode.forEach(element => {
+      $el.appendChild(createElement(element));
+    });
+    return $el;
+  }
+
+  if (typeof vNode.type === 'function') {
+    const $child = vNode.type(vNode.props);
+    $el = createElement({ type: $child.type, props: $child.props, children: $child.children });
+    return $el;
+  }
+
+  $el = document.createElement(vNode.type);
+
+  for (let name in vNode.props) {
+    let propsName = name;
+    if (name === 'className') {
+      propsName = 'class';
+    }
+    if (events.includes(name) && typeof vNode.props[name] === 'function') {
+      const eventName = name.charAt(2).toLocaleLowerCase() + name.slice(3);
+      $el.addEventListener(eventName, vNode.props[name]);
+    }
+    $el.setAttribute(propsName, vNode.props[name]);
+  }
+
+  vNode.children
+    .filter(child => child !== undefined)
+    .forEach(child => {
+      const $child = createElement(child);
+      $el.appendChild($child);
+    });
+
+  return $el;
 }

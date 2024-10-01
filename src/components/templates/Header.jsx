d4030880cc8c18e02b04c926fe8b91288527fba0
ms-bang <ms.bang@teamsparta.co>
2024-10-01 21:05:55 +0900

    feat: 🎸 페이지 렌더링되게 완성

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,4 +1,11 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
-
-export const Header = () => ({});
+import { createVNode } from "../../lib";
+export const Header = () => 
+  (
+    <header class="bg-blue-600 text-white p-4 sticky top-0">
+      <h1 class="text-2xl font-bold">항해플러스</h1>
+    </header>
+  );
+  
+  
+;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,29 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { createVNode } from "../lib";
+import { globalStore } from "../stores/globalStore";
+import { Header } from "../components/templates/Header";
+import { Footer } from "../components/templates/Footer";
+import { Navigation } from "../components/templates/Navigation";
+import { Post } from "../components";
+import { NotFoundPage } from "./NotFoundPage";
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+  return (
+    <div class="bg-gray-100 min-h-screen flex justify-center">
+      <div class="max-w-md w-full">
+        <Header />
+        <Navigation loggedIn={loggedIn} />
+        <main class="p-4">
+          {loggedIn ? <NotFoundPage /> : ""}
+          <div id="posts-container" class="space-y-4">
+            {posts.map(({ author, content, id, time }) => (
+              <Post author={author} content={content} id={id} time={time} />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};

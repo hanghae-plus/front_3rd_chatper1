225ea7f7db12aa0fb974f5bd41bab3ac33073384
남균 <gyuni9604@naver.com>
2024-10-01 18:16:32 +0900

    📝 fix : PostForm 로그인에 따라 랜더링 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,19 @@
 /** @jsx createVNode */
-import { Footer, Post, Navigation, Header } from "../components"
+import { Footer, Post, Navigation, Header, PostForm } from "../components"
 import { createVNode } from "../lib"
 import { globalStore } from "../stores"
 
 export const HomePage = () => {
-  const { posts } = globalStore.getState()
+  const { posts, loggedIn } = globalStore.getState()
 
   return (
-    <div>
+    <div class="bg-gray-100 min-h-screen flex justify-center">
       <div className="max-w-md w-full">
         <Header />
         <Navigation />
         <main className="p-4">
           <div id="posts-container" class="space-y-4">
+            {loggedIn && <PostForm />}
             {posts.map(({ author, time, content, id }) => (
               <Post author={author} time={time} content={content} id={id} />
             ))}

03bfb9aa2c6b76cc67721e302102802791d2f8b3
John-Kim93 <dhkdhd123@naver.com>
2024-10-01 20:19:22 +0900

    FEAT : jsx 컴포넌트 생성

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,27 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { Post } from "../components/posts/Post";
+import { Footer } from "../components/templates/Footer";
+import { Header } from "../components/templates/Header";
+import { Navigation } from "../components/templates/Navigation";
+import { createVNode } from "../lib";
+import { globalStore } from "../stores";
+import { NotFoundPage } from "./NotFoundPage";
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+  return (
+    <div class="bg-gray-100 min-h-screen flex justify-center">
+      <div class="max-w-md w-full">
+        {Header()}
+        {Navigation({ loggedIn })}
+        <main class="p-4">
+          {loggedIn ? NotFoundPage() : ""}
+          <div id="posts-container" class="space-y-4">
+            {posts.map(Post)}
+          </div>
+        </main>
+        {Footer()}
+      </div>
+    </div>
+  );
+};

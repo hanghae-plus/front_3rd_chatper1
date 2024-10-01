2469b82b27295159b78aa8ca0a8a6b4487ca08d3
eggmun98 <eggmun98@gmail.com>
2024-10-01 22:05:42 +0900

    feat: Navigation 컴포넌트 구현

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,20 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
 
-export const Navigation = () => ({});
+const getNavItemClass = (path) => {
+  const currentPath = window.location.pathname;
+  return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+};
+
+export const Navigation = ({ loggedIn }) => `
+   <nav class="bg-white shadow-md p-2 sticky top-14">
+    <ul class="flex justify-around">
+      <li>
+        <a href="/" class="text-blue-600 font-bold" data-link="true">홈</a>
+      </li>
+      <li>
+        <a href="/login" class="text-gray-600" data-link="true">로그인</a>
+      </li>
+    </ul>
+  </nav>
+`;

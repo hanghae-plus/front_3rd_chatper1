ab89676ce5cf064257df827fd05bd6218550d65d
oStone <lws2269@kakao.com>
2024-10-01 22:36:58 +0900

    chore: 템플릿변경에 따른 notfound -> postform

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@
 import { Navigation } from '../components/templates/Navigation';
 import { Header } from '../components/templates/Header';
 import { Footer } from '../components/templates/Footer';
-import { Post } from '../components/posts/Post';
+import { Post, PostForm } from '../components/posts';
 import { globalStore } from '../stores/globalStore';
 import { createVNode } from '../lib';
 
@@ -16,7 +16,7 @@ export const HomePage = () => {
         <Navigation loggedIn={loggedIn} />
 
         <main className='p-4'>
-          {loggedIn ? <NotFoundPage /> : null}
+          {loggedIn ? <PostForm /> : null}
           <div id='posts-container' className='space-y-4'>
             {posts.map((post, index) => (
               <Post key={index} {...post} /> // 각 post 객체의 속성을 props로 전달

7f379b7c9b801b32b14740f462ade173c961d337
Eunsu Lim <limes1787@gmail.com>
2024-10-02 02:23:35 +0900

    feat: createVNode, createElement 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -8,7 +8,50 @@
 //    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
 
-export function createElement(vNode) {
-  // 여기에 구현하세요
-  return {}
+export function createElement(vNode) {\
+  if (!vNode) {
+    return document.createTextNode('');
+  }
+
+  if (typeof vNode === 'string' || typeof vNode === 'number') {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach(child => {
+      fragment.appendChild(createElement(child)); 
+    });
+    return fragment; 
+  }
+
+  if (typeof vNode.type === 'function') {
+    const result = vNode.type(vNode.props || {});
+    
+    return createElement(result);
+  }
+
+  const $el = document.createElement(vNode.type);  
+
+  Object.entries(vNode.props ?? {}).forEach(([key, value]) => {
+    if (key.startsWith('on')) {
+        const eventType = key.slice(2).toLowerCase(); 
+        $el.addEventListener(eventType, (e) => {
+          e.preventDefault();           
+          value(e); 
+        });
+    } else if (key === 'className') {
+      $el.setAttribute('class', value)
+    } else {
+      $el.setAttribute(key, value);
+    }    
+  });
+
+  if (vNode.children) {
+    vNode.children.forEach((child) => {
+      $el.appendChild(createElement(child));
+    });
+  }
+
+  return $el;
 }

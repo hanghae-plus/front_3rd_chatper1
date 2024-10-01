d0e803bf44b88c92191ffe373d456e080b44c796
Eunsu Lim <limes1787@gmail.com>
2024-10-02 02:25:03 +0900

    fix: 오타 수정

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -8,7 +8,7 @@
 //    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
 //    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
 
-export function createElement(vNode) {\
+export function createElement(vNode) {
   if (!vNode) {
     return document.createTextNode('');
   }

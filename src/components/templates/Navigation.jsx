c97225d929629d373d3f46ff54b2c08d7645310e
wktaylor <wktaylorla@gmail.com>
2024-10-01 22:09:04 +0900

    🚧 components

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,52 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
-export const Navigation = () => ({});
+export const Navigation = () => {
+  const { loggedIn } = globalStore.getState();
+  return (
+    <nav className="bg-white shadow-md p-2 sticky top-14">
+      <ul className="flex justify-around">
+        <li>
+          <a href="/" className={getNavItemClassName("/")} data-link>
+            홈
+          </a>
+        </li>
+        {!loggedIn && (
+          <li>
+            <a
+              href="/login"
+              className={getNavItemClassName("/login")}
+              data-link
+            >
+              로그인
+            </a>
+          </li>
+        )}
+        {loggedIn && (
+          <li>
+            <a
+              href="/profile"
+              className={getNavItemClassName("/profile")}
+              data-link
+            >
+              프로필
+            </a>
+          </li>
+        )}
+        {loggedIn && (
+          <li>
+            <a href="#" id="logout" className="text-gray-600">
+              로그아웃
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+const getNavItemClassName = (path) => {
+  const currentPath = window.location.pathname;
+  return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
+};

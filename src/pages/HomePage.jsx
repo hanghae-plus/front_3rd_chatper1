b1f9234770eebf054b0ece65615834b82b4274f1
Bong <borobong230@gmail.com>
2024-10-02 00:39:33 +0900

    :hammer: [Update] template.md 파일 일괄 적용

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,20 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
-
-export const HomePage = () => ({});
+import { createVNode } from "../lib";
+import { globalStore } from "../stores";
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+  return (
+    <div class="bg-gray-100 min-h-screen flex justify-center">
+      <div class="max-w-md w-full">
+        ${Header()} ${Navigation({ loggedIn })}
+        <main class="p-4">
+          ${loggedIn ? PostForm() : ""}
+          <div id="posts-container" class="space-y-4">
+            ${posts.map(Post).join("")}
+          </div>
+        </main>
+        ${Footer()}
+      </div>
+    </div>
+  );
+};

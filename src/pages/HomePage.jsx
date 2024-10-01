a36e9a62c8cac88d94e2c897f50001c35475ba70
codeeon <yeon.develop@gmail.com>
2024-10-01 22:55:43 +0900

    Feat: jsx files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,33 @@
 /** @jsx createVNode */
-import{ createVNode } from "../lib";
+import { createVNode } from '../lib';
+import { globalStore } from '../stores';
+import { Header, Navigation, Post, Footer } from '../components';
+import { NotFoundPage } from '../pages';
 
-export const HomePage = () => ({});
+export const HomePage = () => {
+  const { loggedIn, posts } = globalStore.getState();
+
+  return (
+    <div className='bg-gray-100 min-h-screen flex justify-center'>
+      <div className='max-w-md w-full'>
+        <Header />
+        <Navigation loggedIn={loggedIn} />
+        <main className='p-4'>
+          {loggedIn ? <NotFoundPage /> : ''}
+          <div id='posts-container' className='space-y-4'>
+            {posts.map((post) => (
+              <Post
+                author={post.author}
+                time={post.time}
+                content={post.content}
+                id={post.id}
+                key={post.id}
+              />
+            ))}
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};

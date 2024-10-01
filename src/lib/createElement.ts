86ec839fdb175b5ef0bbe49a98b342beadaae74e
정원 <jwha94@gmail.com>
2024-10-01 12:43:05 +0900

    feat: createElement 작업 완료

---
+++ src/lib/createElement.ts
@@ -0,0 +1,54 @@
+import type { VNode, VNodeChildren, VNodeProps } from './createVNode';
+
+function setAttributes($element: HTMLElement, props: NonNullable<VNodeProps>) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (typeof value === 'function' && key.startsWith('on')) {
+      $element.addEventListener(key.slice(2).toLowerCase(), value);
+    } else if (key === 'className') {
+      $element.classList.add(value);
+    } else {
+      $element.setAttribute(key, value);
+    }
+  });
+}
+
+function appendChildren($element: HTMLElement, children: VNodeChildren) {
+  for (const child of children) {
+    $element.appendChild(createElement(child));
+  }
+}
+
+/**
+ * 주어진 가상 DOM 노드(vNode)를 실제 DOM 노드로 변환하여 반환합니다.
+ * @param {VNode} vNode - 가상 DOM 노드
+ * @returns {Node} 변환된 DOM 노드
+ */
+export function createElement(vNode: VNode): Node {
+  if (!vNode) return document.createTextNode('');
+
+  if (typeof vNode === 'string' || typeof vNode === 'number') {
+    return document.createTextNode(String(vNode));
+  }
+
+  if (Array.isArray(vNode)) {
+    const $fragment = document.createDocumentFragment();
+    vNode.forEach((child) => $fragment.appendChild(createElement(child)));
+    return $fragment;
+  }
+
+  if (typeof vNode.type === 'function') {
+    return createElement(vNode.type(vNode.props));
+  }
+
+  const $element = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    setAttributes($element, vNode.props);
+  }
+
+  if (vNode.children) {
+    appendChildren($element, vNode.children);
+  }
+
+  return $element;
+}
--- src/lib/createVNode.ts
+++ src/lib/createVNode.ts
@@ -1,11 +1,20 @@
-type VNode = {
-  type: string;
-  props: Record<string, any> | null;
-  children: any[];
+export type VNodeType = Function | keyof HTMLElementTagNameMap;
+export type VNodeProps = Record<string, any> | null;
+export type VNodeChildren = VNode[];
+
+type VNodeElement = {
+  type: VNodeType;
+  props: VNodeProps;
+  children: VNodeChildren;
 };
 
+export type VNode = VNodeElement | string | number | any[];
+
+/**
+ * 주어진 타입, 속성, 자식 요소들로 가상 DOM 노드(VNode)를 생성합니다.
+ */
 export function createVNode(
-  type: string,
+  type: VNodeType,
   props: Record<string, any> | null,
   ...children: any[]
 ): VNode {

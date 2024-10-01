5100ac0c355d7e0ea200ef5a96f66f5cabcf8a2a
temeraire97 <hyensooyoon@gmail.com>
2024-10-01 21:18:08 +0900

    UPDATE: 코멘트 수정

--- src/lib/createElement__v2.js
+++ src/lib/createElement__v2.js
@@ -1,5 +1,4 @@
-export function createElement__v2(vNode) {
-  // 이 함수는 createElement의 개선된 버전입니다.
+// TODO: 이 함수는 createElement의 개선된 버전입니다.
 // 1. falsy vNode 처리
 // 2. 문자열 또는 숫자 vNode 처리
 // 3. 배열 vNode 처리 (DocumentFragment 사용)
@@ -7,5 +6,7 @@ export function createElement__v2(vNode) {
 //    - 요소 생성
 //    - 속성 설정 (이벤트 함수를 이벤트 위임 방식으로 등록할 수 있도록 개선)
 //    - 자식 요소 추가
-  return {}
+
+export function createElement__v2(vNode) {
+  return {};
 }

83c8d9cf0188b7c2eb1af5821431230232f2007f
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-02 11:29:39 +0900

    ♻️ refactor :: 외부에서 로그인 상태를 처리하도록, 조건문을 간결하게 리팩토링

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,51 +1,40 @@
-/** @jsx createVNode */
-import { createVNode } from "../../lib";
-import { userStorage } from "../../storages";
+import { createVNode } from '../../lib';
 
-export const Navigation = () => {
-  const loggedIn = userStorage.get("user");
+const  getNavigateList = (path) => {
+  const currentPath = window.location.pathname;
+  return currentPath === path ? 'text-blue-600 font-bold' : 'text-gray-600';
+};
 
-  const getNavigateList = (href) => {
-    return location.pathname == href
-      ? "text-blue-600 font-bold"
-      : "text-gray-600";
-  };
-
+export const Navigation = ({ loggedIn }) => {
   return (
-    <nav class="bg-white shadow-md p-2 sticky top-14">
-      <ul class="flex justify-around">
+    <nav className="bg-white shadow-md p-2 sticky top-14">
+      <ul className="flex justify-around">
         <li>
-          <a href="/" class={getNavigateList("/")} data-link>
+          <a href="/" className={getNavigateList('/')} data-link>
             홈
           </a>
         </li>
-        {loggedIn ? (
-          <li>
-            <a href="/profile" class={getNavigateList("/profile")} data-link>
-              프로필
-            </a>
-          </li>
-        ) : (
-          ''
-        )}
         {!loggedIn ? (
           <li>
-            <a href="/login" class={getNavigateList("/login")} data-link>
+            <a href="/login" className={getNavigateList('/login')} data-link>
               로그인
             </a>
           </li>
-        ) : (
-          ''
-        )}
+        ) : null}
         {loggedIn ? (
           <li>
-            <a href="#" id="logout" class="text-gray-600">
+            <a href="/profile" className={getNavigateList('/profile')} data-link>
+              프로필
+            </a>
+          </li>
+        ) : null}
+        {loggedIn ? (
+          <li>
+            <a href="#" id="logout" className="text-gray-600">
               로그아웃
             </a>
           </li>
-        ) : (
-          ''
-        )}
+        ) : null}
       </ul>
     </nav>
   );

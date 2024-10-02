bc9507a1115b3f2401fae9bdd17cdb128e5c3f5d
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-02 10:53:28 +0900

    ✨ feat :: renderelment가 에러 바운더리를 처리를 할 수 있도록 타켓 컴포넌트화 조건부 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 /** @jsx createVNode */
 import { createVNode } from "./lib";
 import { globalStore } from "./stores";
+import { NotFoundPage } from "./pages";
 
 export const App = ({ targetPage }) => {
+  const AppComponent = targetPage ?? NotFoundPage;
   const error = globalStore.getState().error;
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
+    <div>
+      <AppComponent />
       {error ? (
-        <p id="error-boundary" class="font-bold p-10 text-red-600" role="alert">
-          오류 발생! ${error.message || '알 수 없는 오류가 발생했습니다.'}
-        </p>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
+          <p
+            id="error-boundary"
+            class="font-bold p-10 text-red-600"
+            role="alert"
+          >
+            오류 발생! ${error.message || "알 수 없는 오류가 발생했습니다."}
+          </p>
+        </div>
       ) : (
-        targetPage()
+        ""
       )}
     </div>
   );

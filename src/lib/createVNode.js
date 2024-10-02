4e0f107dd57018088f10e9a927c8fbabfd4f585c
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2024-10-03 08:13:28 +0900

    createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -4,7 +4,14 @@
 // 3. children은 평탄화(flat)되어야 하며, falsy 값은 필터링되어야 합니다.
 // 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
 
+//jsx를 가상돔 구조로 변환
 export function createVNode(type, props, ...children) {
-  // 여기에 구현하세요
-  return {}
+  const vNode = {
+    type,
+    props,
+    children: children.flat(Infinity).filter(Boolean),
+  };
+
+  //평탄화
+  return vNode;
 }

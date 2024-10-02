00ee115147c415b77bc3d295d941553e838b7ca3
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-02 17:32:34 +0900

    ✨ feat :: renderElement 변환으로 인한 라우터에 notFound 라우팅 처리 구현

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,12 +1,11 @@
 import { createObserver } from "./createObserver";
+import { NotFoundPage } from '../pages';
 
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
   const getTarget = () =>
-    routes[window.location.pathname]
-      ? routes[window.location.pathname]
-      : routes['/404']
+    routes[window.location.pathname] ?? NotFoundPage
 
   const push = (path) => {
     window.history.pushState(null, null, path);

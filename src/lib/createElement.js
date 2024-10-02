5c973330e4ca202000244680fd52528a4f066a47
Gred <jihong.park.dev@gmail.com>
2024-10-02 14:29:04 +0900

    feat: createElement, createVNode 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,14 +1,51 @@
-// TODO: createElement 함수 구현
-// 1. vNode가 falsy면 빈 텍스트 노드를 반환합니다.
-// 2. vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
-// 3. vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
-// 4. vNode.type이 함수면 해당 함수를 호출하고 그 결과로 createElement를 재귀 호출합니다.
-// 5. 위 경우가 아니면 실제 DOM 요소를 생성합니다:
-//    - vNode.type에 해당하는 요소를 생성
-//    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
-//    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
+import { isEmptyObject } from '../utils';
 
 export function createElement(vNode) {
-  // 여기에 구현하세요
-  return {}
+	console.log('vNode: ', vNode);
+	// vNode가 falsy면 빈 텍스트 노드를 반환합니다.
+	if (!vNode || isEmptyObject(vNode)) {
+		return document.createTextNode('');
+	}
+
+	// vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
+	if (typeof vNode === 'string' || typeof vNode === 'number') {
+		return document.createTextNode(`${vNode}`);
+	}
+
+	// vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
+	if (Array.isArray(vNode)) {
+		const fragment = document.createDocumentFragment();
+		vNode.forEach((child) => {
+			fragment.appendChild(createElement(child));
+		});
+		return fragment;
+	}
+
+	if (typeof vNode.type === 'function') {
+		return createElement(vNode.type(vNode.props || {}));
+	}
+
+	// 실제 DOM 요소를 생성
+	// vNode.type에 해당하는 요소를 생성
+	const element = document.createElement(vNode.type);
+	console.log('element: ', element);
+
+	// vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
+	Object.entries(vNode.props || {}).forEach(([name, value]) => {
+		console.log('prop', name, value);
+		if (name === 'className') {
+			element.setAttribute('class', value);
+		} else if (name.startsWith('on') && typeof value === 'function') {
+			// 이벤트 리스너 추가
+			const eventType = name.slice(2).toLowerCase();
+			element.addEventListener(eventType, value);
+		} else {
+			element.setAttribute(name, value);
+		}
+	});
+
+	// vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
+	vNode.children?.forEach((child) => element.appendChild(createElement(child)));
+
+	return element;
 }

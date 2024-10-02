78217797e6063b814fd55390d555387071d8fb3f
Gred <jihong.park.dev@gmail.com>
2024-10-02 21:51:37 +0900

    feat: add template components

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -1,4 +1,42 @@
 /** @jsx createVNode */
-import{ createVNode } from "../../lib";
+/** @jsxFrag Fragment */
+import { createVNode, Fragment } from '../../lib';
 
-export const Navigation = () => ({});
+export const Navigation = ({ loggedIn }) => {
+  const getNavItemClass = (path) => {
+    const currentPath = window.location.pathname;
+    return currentPath === path ? 'text-blue-600 font-bold' : 'text-gray-600';
+  };
+
+  return (
+    <nav className="bg-white shadow-md p-2 sticky top-14">
+      <ul className="flex justify-around">
+        <li>
+          <a href="/" className={getNavItemClass('/')} data-link>
+            홈
+          </a>
+        </li>
+        {loggedIn ? (
+          <>
+            <li>
+              <a href="/profile" className={getNavItemClass('/profile')} data-link>
+                프로필
+              </a>
+            </li>
+            <li>
+              <a href="#" id="logout" className="text-gray-600">
+                로그아웃
+              </a>
+            </li>
+          </>
+        ) : (
+          <li>
+            <a href="/login" className={getNavItemClass('/login')} data-link>
+              로그인
+            </a>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};

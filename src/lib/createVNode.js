e10ca6bc12baa1c6140b60e1e75538a716e33722
chyerin802 <chyerin802@gmail.com>
2024-10-02 23:55:54 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -3,8 +3,6 @@
 // 2. 반환값은 { type, props, children } 형태의 객체여야 합니다.
 // 3. children은 평탄화(flat)되어야 하며, falsy 값은 필터링되어야 합니다.
 // 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
-
 export function createVNode(type, props, ...children) {
-  // 여기에 구현하세요
-  return {}
+  return { type, props, children: children.flat(Infinity).filter(Boolean) };
 }

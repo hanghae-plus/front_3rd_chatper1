3ea0ac6a0a21d520634abc0502883ffe1aed3df5
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-02 12:37:00 +0900

    ♻️  refactor :: App내부 div 태그 수정하고, 불필요한 import 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 /** @jsx createVNode */
-import { createVNode } from "./lib";
-import { globalStore } from "./stores";
-import { NotFoundPage } from "./pages";
+import { createVNode } from './lib';
+import { globalStore } from './stores/globalStore';
 
 export const App = ({ targetPage }) => {
   const PageComponent = targetPage ?? NotFoundPage;
@@ -11,20 +10,20 @@ export const App = ({ targetPage }) => {
     <div>
       <PageComponent />
       {error ? (
-       <div id="error-boundary" class="fixed bottom-4 left-4 right-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg shadow-lg transition-opacity duration-300 hover:opacity-75" role="alert">
-       <div class="flex justify-between items-center">
-         <div>
-           <strong class="font-bold">오류 발생!</strong>
-           <span class="block sm:inline ml-1">${error.message || '알 수 없는 오류가 발생했습니다.'}</span>
-         </div>
-         <button class="text-red-700 hover:text-red-900 font-semibold">
-           &times;
-         </button>
-       </div>
-     </div>
-      ) : (
-        ""
-      )}
+        <div
+          id="error-boundary"
+          className="fixed bottom-4 left-4 right-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg shadow-lg transition-opacity duration-300 hover:opacity-75"
+          role="alert"
+        >
+          <div className="flex justify-between items-center">
+            <div>
+              <strong className="font-bold">오류 발생!</strong>
+              <span className="block sm:inline ml-1">${error.message || '알 수 없는 오류가 발생했습니다.'}</span>
+            </div>
+            <button className="text-red-700 hover:text-red-900 font-semibold">&times;</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };

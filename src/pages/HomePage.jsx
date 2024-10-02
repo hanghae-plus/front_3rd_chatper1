0206db49268b60bbe22261e657f4831a727de8c7
Bong <borobong230@gmail.com>
2024-10-03 01:07:04 +0900

    :hammer: [Update] HomePage.jsx 오류 수정 ($ 제거, 임포트 추가, 프로퍼티 추가) #chapter1-2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,32 @@
 /** @jsx createVNode */
 import { createVNode } from "../lib";
 import { globalStore } from "../stores";
+import { Header, Footer, Navigation, PostForm, Post } from "../components";
+
 export const HomePage = () => {
   const { loggedIn, posts } = globalStore.getState();
+
   return (
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
-        ${Header()} ${Navigation({ loggedIn })}
+        <Header />
+        <Navigation loggedIn={loggedIn} />
         <main class="p-4">
-          ${loggedIn ? PostForm() : ""}
+          {loggedIn ? <PostForm /> : ""}
           <div id="posts-container" class="space-y-4">
-            ${posts.map(Post).join("")}
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                post={post}
+                author={post.author}
+                time={post.time}
+                content={post.content}
+                id={post.id}
+              />
+            ))}
           </div>
         </main>
-        ${Footer()}
+        <Footer />
       </div>
     </div>
   );

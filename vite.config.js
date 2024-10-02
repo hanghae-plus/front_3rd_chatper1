240c95947e63d3bcaf594e2fb346995e051f01f0
KimDanwoo <dudgh9198@naver.com>
2024-10-02 17:53:20 +0900

    chore: prettier, vite설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default mergeConfig(
   defineConfig({
@@ -10,14 +11,26 @@ export default mergeConfig(
       esbuildOptions: {
         jsx: 'transform',
         jsxFactory: 'createVNode',
-      }
-    }
+      },
+    },
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, './src/components'),
+        '@lib': path.resolve(__dirname, './src/lib'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@constants': path.resolve(__dirname, './src/constants'),
+        '@stores': path.resolve(__dirname, './src/stores'),
+        '@storages': path.resolve(__dirname, './src/storages'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+        '@errors': path.resolve(__dirname, './src/errors'),
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.js'
+      setupFiles: './src/setupTests.js',
     },
   })
-);
+)

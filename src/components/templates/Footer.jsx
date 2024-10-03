2f4f894d99fcfa0ae2a5ea756935cc677fc0eefd
eggmun98 <eggmun98@gmail.com>
2024-10-03 19:15:32 +0900

    update: 문자열 리터럴에서 jsx로 변경

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,8 +1,8 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
 
-export const Footer = () => `
+export const Footer = () => (
   <footer class="bg-gray-200 p-4 text-center">
     <p>© 2024 항해플러스. All rights reserved.</p>
   </footer>
-`;
+);
--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -6,15 +6,19 @@ const getNavItemClass = (path) => {
   return currentPath === path ? "text-blue-600 font-bold" : "text-gray-600";
 };
 
-export const Navigation = ({ loggedIn }) => `
-   <nav class="bg-white shadow-md p-2 sticky top-14">
+export const Navigation = ({ loggedIn }) => (
+  <nav class="bg-white shadow-md p-2 sticky top-14">
     <ul class="flex justify-around">
       <li>
-        <a href="/" class="text-blue-600 font-bold" data-link="true">홈</a>
+        <a href="/" class="text-blue-600 font-bold" data-link="true">
+          홈
+        </a>
       </li>
       <li>
-        <a href="/login" class="text-gray-600" data-link="true">로그인</a>
+        <a href="/login" class="text-gray-600" data-link="true">
+          로그인
+        </a>
       </li>
     </ul>
   </nav>
-`;
+);

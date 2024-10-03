52c8a03380e32e000305ecdf198bc68639a14523
Gred <jihong.park.dev@gmail.com>
2024-10-04 01:11:59 +0900

    feat: login, profile 수정 이벤트 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,29 @@
 /** @jsx createVNode */
 import { createVNode } from '../lib';
+import { userStorage } from '../storages';
+import { globalStore } from '../stores';
 
 export const LoginPage = () => {
+  const login = (username) => {
+    const user = { username, email: '', bio: '' };
+    globalStore.setState({
+      currentUser: user,
+      loggedIn: true,
+    });
+    userStorage.set(user);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    login(username);
+  };
+
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
-        <form id="login-form">
+        <form id="login-form" onSubmit={handleSubmit}>
           <input type="text" id="username" placeholder="사용자 이름" className="w-full p-2 mb-4 border rounded" required />
           <input type="password" placeholder="비밀번호" className="w-full p-2 mb-6 border rounded" required />
           <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,27 @@
 /** @jsx createVNode */
 import { Header, Navigation, Footer } from '../components';
 import { createVNode } from '../lib';
+import { userStorage } from '../storages';
 import { globalStore } from '../stores';
 
 export const ProfilePage = () => {
   const { loggedIn, currentUser } = globalStore.getState();
   const { username = '', email = '', bio = '' } = currentUser ?? {};
+
+  const updateProfile = (profile) => {
+    const user = { ...globalStore.getState().currentUser, ...profile };
+    globalStore.setState({ currentUser: user });
+    userStorage.set(user);
+
+    alert('프로필이 업데이트되었습니다.');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const updatedProfile = Object.fromEntries(formData);
+    updateProfile(updatedProfile);
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen flex justify-center">
@@ -15,7 +31,7 @@ export const ProfilePage = () => {
         <main className="p-4">
           <div className="bg-white p-8 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold text-center text-blue-600 mb-8">내 프로필</h2>
-            <form id="profile-form">
+            <form id="profile-form" onSubmit={handleSubmit}>
               <div className="mb-4">
                 <label for="username" className="block text-gray-700 text-sm font-bold mb-2">
                   사용자 이름

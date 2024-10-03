45e7a88323bd1ecb14544d2c2c89c3190d1087fb
younu <yun-woojung@younuui-MacBookPro.local>
2024-10-03 20:31:56 +0900

    feat: router 예외처리 추가

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,16 +1,23 @@
 import { createObserver } from "./createObserver";
 
 export const createRouter = (routes) => {
+  //console.log("routes", routes);
   const { subscribe, notify } = createObserver();
 
-  const getTarget = () => routes[window.location.pathname]
+  //존재하지 않는 경로로 접근하면 404 페이지가 렌더링된다.
+  const getTarget = () => {
+    //console.log("현재 경로:", window.location.pathname);
+    const target = routes[window.location.pathname];
+    //console.log("타겟 컴포넌트:", target ? target : "404 페이지로 이동");
+    return target ? target : routes["/404"];
+  };
 
   const push = (path) => {
     window.history.pushState(null, null, path);
     notify();
-  }
+  };
 
-  window.addEventListener('popstate', () => notify());
+  window.addEventListener("popstate", () => notify());
 
-  return { push, subscribe, getTarget }
+  return { push, subscribe, getTarget };
 };

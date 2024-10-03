04a6d2ee1e69c5ce99e7d897578ece7f1283ed0c
Harry <harry@bzznbyd.com>
2024-10-03 20:40:39 +0900

    feat: craeteVNode 완성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,10 +1,20 @@
-// TODO: createVNode 함수 구현
-// 1. type, props, ...children을 매개변수로 받는 함수를 작성하세요.
-// 2. 반환값은 { type, props, children } 형태의 객체여야 합니다.
-// 3. children은 평탄화(flat)되어야 하며, falsy 값은 필터링되어야 합니다.
-// 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
+/**
+ * 가상 노드(Virtual DOM)를 생성하는 함수입니다.
+ * 
+ * @param {string|function} type - DOM 요소의 타입 (예: 태그 이름) 또는 함수형 컴포넌트.
+ * @param {object} [props={}] - DOM 요소에 적용할 속성 또는 컴포넌트에 전달할 props.
+ * @param {...any} children - 자식 요소들, 중첩된 배열 형태도 허용.
+ * @returns {object} - 가상 DOM 노드. { type, props, children } 형태의 객체로 반환.
+ * 
+ * 이 함수는 다음과 같은 작업을 수행합니다:
+ * 1. type이 DOM 요소의 태그 이름이거나 함수형 컴포넌트인지 확인합니다.
+ * 2. props는 비어 있을 경우 기본적으로 빈 객체로 설정합니다.
+ * 3. 모든 자식 요소를 flat(Infinity)로 평탄화하며, falsy 값 (null, undefined, false 등)은 필터링하여 제거합니다.
+ */
+export function createVNode(type, props = {}, ...children) {
+  // 자식 요소를 평탄화하고 falsy 값(null, undefined, false)을 제거합니다.
+  const flatChildren = children.flat(Infinity).filter(Boolean);
 
-export function createVNode(type, props, ...children) {
-  // 여기에 구현하세요
-  return {}
+  // 가상 DOM 노드를 반환합니다.
+  return { type, props, children: flatChildren };
 }

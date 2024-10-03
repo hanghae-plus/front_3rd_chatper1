235f35eadbafa17c5d12df8330f3e2e3aa12b209
sanghyuk-2i <desummit1@gmail.com>
2024-10-03 22:56:43 +0900

    Fix: 가상 DOM 로직 수정
    - 발제 자료 기반으로 재수정

--- src/utils/dom.js
+++
@@ -1,9 +0,0 @@
-export function flattenChildren(children) {
-  return children.reduce(
-    (flatted, child) =>
-      Array.isArray(child)
-        ? [...flatted, ...flattenChildren(child)]
-        : [...flatted, child],
-    []
-  );
-}

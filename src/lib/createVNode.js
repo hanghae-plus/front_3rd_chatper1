33368b8f47bb74ed737c4aee436d5e0734a937d0
Ensil <lasags@naver.com>
2024-10-03 23:27:30 +0900

    구현) createVNode: Falsy한 값 제거

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -4,6 +4,6 @@
 // 3. children은 평탄화(flat)되어야 하며, falsy 값은 필터링되어야 합니다.
 // 4. Infinity를 사용하여 모든 깊이의 배열을 평탄화하세요.
 
-export function createVNode(type, props, ...children) {
-    return { type: type, props: props, children: children.flat(Infinity) };
-}
+export function createVNode(type, props = {}, ...children) {
+    return { type, props, children: children.flat(Infinity).filter(Boolean) };
+  }

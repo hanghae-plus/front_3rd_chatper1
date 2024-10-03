f89cf6e3903d2779757817a699a13f638f1d2878
ooyuo <doyounjeong0@gmail.com>
2024-10-03 18:52:25 +0900

    fit: Homepage 에서 postform 삭제

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
 import { createVNode } from "../lib";
 import { globalStore } from "../stores";
-import React from "react";
 
 export const HomePage = () => {
   const { loggedIn, posts } = globalStore.getState();
@@ -13,7 +12,6 @@ export const HomePage = () => {
         <Navigation loggedIn={loggedIn} />
         <main class="p-4">
           <div id="posts-container" class="space-y-4">
-            {/* <PostForm /> */}
             {posts.map((post) => (
               <Post key={post.id} {...post} />
             ))}

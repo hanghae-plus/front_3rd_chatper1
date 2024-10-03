0f60c45141f91319ee7c2b8514c4a63bb2c266aa
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-04 00:24:11 +0900

    fix: 로그인 상태처리 및 컴포넌트 호출방식 변경

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,27 @@
 /** @jsx createVNode */
 import { createVNode } from "../lib";
+
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
 import { globalStore } from "../stores";
 
 export const HomePage = () => {
-    const { loggedIn, currentUser, posts } = globalStore.getState();
+    const { loggedIn, posts } = globalStore.getState();
     return (
         <div class="bg-gray-100 min-h-screen flex justify-center">
             <div class="max-w-md w-full">
-                {Header()}
-                {Navigation({ loggedIn })}
+                <Header />
+                <Navigation loggedIn={loggedIn} />
+
                 <main class="p-4">
                     {loggedIn ? PostForm() : ""}
                     <div id="posts-container" class="space-y-4">
-                        {posts.map(Post)}
+                        {posts.map(({ id, author, time, content }) => {
+                            return <Post id={id} author={author} time={time} content={content} />;
+                        })}
                     </div>
                 </main>
-                {Footer()}
+
+                <Footer />
             </div>
         </div>
     );

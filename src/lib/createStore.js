fd43cff35f595eb56b061de8ebbf86ee19032f7d
lsw <hkbvc1222@gmail.com>
2024-10-04 04:22:52 +0900

    perf: store 값 변화 있을 때만 옵저버 실행

--- src/lib/createStore.js
+++ src/lib/createStore.js
@@ -1,4 +1,4 @@
-import { createObserver } from "./createObserver.js";
+import { createObserver } from './createObserver.js';
 
 export const createStore = (initialStore) => {
   const { subscribe, notify } = createObserver();
@@ -6,11 +6,41 @@ export const createStore = (initialStore) => {
   let state = { ...initialStore };
 
   const setState = (newState) => {
-    state = { ...state, ...newState }
-    notify()
-  }
+    const changedState = { ...state, ...newState };
+    const isEqual = deepEqual(state, changedState);
+
+    if (isEqual) return;
+
+    state = changedState;
+    notify();
+  };
 
   const getState = () => ({ ...state });
 
-  return { getState, setState, subscribe }
+  return { getState, setState, subscribe };
+};
+
+function deepEqual(obj1, obj2) {
+  // 참조값이 같으면 true
+  if (obj1 === obj2) return true;
+
+  // 두 값이 객체가 아니거나 한쪽만 객체라면 false
+  if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+    return false;
+  }
+
+  // 객체의 키를 비교
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  // 각 키에 대해 재귀적으로 비교
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
 }

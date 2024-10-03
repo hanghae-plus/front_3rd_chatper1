1101d865accf9241a3d3f5b009f4c5d00f0b7da4
Gred <jihong.park.dev@gmail.com>
2024-10-03 13:22:58 +0900

    feat: add ErrorBoundary component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 /** @jsx createVNode */
-import { createVNode } from './lib';
+/** @jsxFrag Fragment */
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { createVNode, Fragment } from './lib';
+import { NotFoundPage } from './pages';
+import { globalStore } from './stores';
 
 export const App = ({ targetPage }) => {
-  const Page = targetPage;
-  return <Page />;
+  const Page = targetPage || NotFoundPage;
+  const error = globalStore.getState().error;
+
+  return (
+    <>
+      <Page />
+      {error && <ErrorBoundary message={error.message} />}
+    </>
+  );
 };

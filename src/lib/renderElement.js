97adf1108e9523040af9c46ee212325e6e7498e3
doitidey <evxrafter@gmail.com>
2024-10-03 22:58:30 +0900

    Feature: renderElement processVNode 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,18 +1,39 @@
 // renderElement.js
-import { addEvent, removeEvent, setupEventListeners } from './eventManager';
+import { addEvent, removeEvent, setupEventListeners } from "./eventManager";
 import { createElement__v2 } from "./createElement__v2.js";
 
 // TODO: processVNode 함수 구현
-function processVNode() {
+function processVNode(vNode) {
   // vNode를 처리하여 렌더링 가능한 형태로 변환합니다.
   // - null, undefined, boolean 값 처리
   // - 문자열과 숫자를 문자열로 변환
   // - 함수형 컴포넌트 처리 <---- 이게 제일 중요합니다.
   // - 자식 요소들에 대해 재귀적으로 processVNode 호출
+
+  // 오답노트
+  // createElement와 비슷한 처리를 하는듯 보이지만 노드 반환 없음
+  // vNode를 형태 가공하는 작업
+  // 0도 falsy값이라는 것을 잊지 말 것,,,
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  } else if (
+    vNode === null ||
+    vNode === undefined ||
+    typeof vNode === "boolean"
+  ) {
+    return "";
+  } else if (typeof vNode.type === "function") {
+    return processVNode(vNode.type(vNode.props));
+  }
+  vNode.children.forEach((childVNode) => {
+    processVNode(childVNode);
+  });
+  return vNode;
 }
 
 // TODO: updateAttributes 함수 구현
-function updateAttributes() {
+function updateAttributes(element, prevProps, newProps) {
   // DOM 요소의 속성을 업데이트합니다.
   // - 이전 props에서 제거된 속성 처리
   // - 새로운 props의 속성 추가 또는 업데이트
@@ -27,25 +48,19 @@ function updateAttributes() {
 function updateElement() {
   // 1. 노드 제거 (newNode가 없고 oldNode가 있는 경우)
   // TODO: oldNode만 존재하는 경우, 해당 노드를 DOM에서 제거
-
   // 2. 새 노드 추가 (newNode가 있고 oldNode가 없는 경우)
   // TODO: newNode만 존재하는 경우, 새 노드를 생성하여 DOM에 추가
-
   // 3. 텍스트 노드 업데이트
   // TODO: newNode와 oldNode가 둘 다 문자열 또는 숫자인 경우
   // TODO: 내용이 다르면 텍스트 노드 업데이트
-
   // 4. 노드 교체 (newNode와 oldNode의 타입이 다른 경우)
   // TODO: 타입이 다른 경우, 이전 노드를 제거하고 새 노드로 교체
-
   // 5. 같은 타입의 노드 업데이트
   // 5-1. 속성 업데이트
   // TODO: updateAttributes 함수를 호출하여 속성 업데이트
-
   // 5-2. 자식 노드 재귀적 업데이트
   // TODO: newNode와 oldNode의 자식 노드들을 비교하며 재귀적으로 updateElement 호출
   // HINT: 최대 자식 수를 기준으로 루프를 돌며 업데이트
-
   // 5-3. 불필요한 자식 노드 제거
   // TODO: oldNode의 자식 수가 더 많은 경우, 남은 자식 노드들을 제거
 }
@@ -55,7 +70,6 @@ export function renderElement(vNode, container) {
   // 최상위 수준의 렌더링 함수입니다.
   // - 이전 vNode와 새로운 vNode를 비교하여 업데이트
   // - 최초 렌더링과 업데이트 렌더링 처리
-
   // 이벤트 위임 설정
   // TODO: 렌더링이 완료된 후 setupEventListeners 함수를 호출하세요.
   // 이는 루트 컨테이너에 이벤트 위임을 설정하여 모든 하위 요소의 이벤트를 효율적으로 관리합니다.

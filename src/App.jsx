96df4bcc4dad3f21d2e30afd0c500fc8f9188f07
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-04 01:43:32 +0900

    feat :App error 분기처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@
 import { createVNode } from "./lib";
 
 import { RootLayout } from "@/components";
+import { globalStore } from "./stores";
+import { ErrorPage } from "./pages";
 
 export function App(props) {
   const { targetPage } = props || {};
-  return <RootLayout>{targetPage()}</RootLayout>;
+  const { error } = globalStore.getState();
+
+  return <RootLayout>{!error ? targetPage() : <ErrorPage />}</RootLayout>;
 }

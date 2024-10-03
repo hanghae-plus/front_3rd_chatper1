4fb222476298935eefcec568236b74994a3646f0
John-Kim93 <dhkdhd123@naver.com>
2024-10-03 23:00:18 +0900

    FEAT : processVNode 구현
    - vNode를 완전히 객체 형태의 비교가 가능한 형태로 변환

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,6 +3,29 @@ import { createElement__v2 } from "./createElement__v2.js";
 import { elementToVNode } from "./createVNode.js";
 import { isNullish, isTextNode } from "../utils/typeUtils.js";
 
+function processVNode(vNode) {
+  // 자기 자신에 대한 처리
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return null;
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+  if (typeof vNode.type === "function") {
+    return processVNode(vNode.type(vNode.props));
+  }
+
+  // 자식 노드에 대한 처리
+  if (vNode.children.length > 0) {
+    const { type, props } = vNode;
+    const processedChildren = vNode.children.map((child) =>
+      processVNode(child)
+    );
+    return { type, props, children: processedChildren };
+  }
+
+  return vNode;
+}
 
 export function renderElement(vNode, container) {
   if (!container) return;

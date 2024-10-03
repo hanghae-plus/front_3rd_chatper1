e06b2267323809937ed7743027e3bf17f69ec3a8
eggmun98 <eggmun98@gmail.com>
2024-10-03 20:50:05 +0900

    feat: 로그인 함수 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,30 +1,53 @@
 /** @jsx createVNode */
+
+import { userStorage } from "../storages";
+import { globalStore } from "../stores";
 import { createVNode } from "../lib";
 
 export const LoginPage = () => {
+  const onClickLogin = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    const { username } = data;
+
+    userStorage.set({
+      username,
+      email: "",
+      bio: "",
+    });
+
+    globalStore.setState({
+      currentUser: userStorage.get(),
+      loggedIn: true,
+    });
+  };
+
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold text-center text-blue-600 mb-8">
           항해플러스
         </h1>
-        <form id="login-form">
+        <form id="login-form" onSubmit={onClickLogin}>
           <input
             type="text"
             id="username"
+            name="username"
             placeholder="사용자 이름"
-            className="w-full p-2 mb-4 border rounded"
+            class="w-full p-2 mb-4 border rounded"
             required
           />
           <input
             type="password"
             placeholder="비밀번호"
-            className="w-full p-2 mb-6 border rounded"
+            class="w-full p-2 mb-6 border rounded"
             required
           />
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white p-2 rounded"
+            class="w-full bg-blue-600 text-white p-2 rounded"
           >
             로그인
           </button>

382c77869f7f01a9ada4ecf9e99c114f4c88f5fa
Choi A Ram <83874298+rami0617@users.noreply.github.com>
2024-10-04 10:19:15 +0900

    [style]: 네비게이션 바 스타일 업데이트

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -4,42 +4,43 @@ import { logout } from '@/main';
 
 export const Navigation = ({ loggedIn }) => {
   const getClassName = (path) => {
-    if (path === window.location.pathname) {
-      return 'text-blue-600 font-bold';
-    } else {
-      return 'text-gray-600';
-    }
+    return path === window.location.pathname ? 'text-blue-600 font-bold' : 'text-gray-600';
   };
 
   return (
     <nav className="bg-white shadow-md p-2 sticky top-14">
-      <ul className="flex justify-around">
-        <li>
-          <a href="/" className={getClassName('/')} data-link="true">
-            홈
-          </a>
-        </li>
-        {loggedIn ? (
-          <ul>
-            <li>
-              <a href="/profile" className={getClassName('/profile')} data-link="true">
-                프로필
-              </a>
-            </li>
-            <li>
-              <a id="logout" href="#" className="text-gray-600" onClick={logout}>
-                로그아웃
-              </a>
-            </li>
-          </ul>
-        ) : (
+      {loggedIn ? (
+        <ul className="flex justify-evenly items-center">
+          <li>
+            <a href="/" className={getClassName('/')} data-link="true">
+              홈
+            </a>
+          </li>
+          <li className="flex space-x-4">
+            <a href="/profile" className={getClassName('/profile')} data-link="true">
+              프로필
+            </a>
+          </li>
+          <li>
+            <a id="logout" href="#" className="text-gray-600" onClick={logout}>
+              로그아웃
+            </a>
+          </li>
+        </ul>
+      ) : (
+        <ul className="flex justify-evenly items-center">
+          <li>
+            <a href="/" className={getClassName('/')} data-link="true">
+              홈
+            </a>
+          </li>
           <li>
             <a href="/login" className="text-gray-600" data-link="true">
               로그인
             </a>
           </li>
-        )}
-      </ul>
+        </ul>
+      )}
     </nav>
   );
 };

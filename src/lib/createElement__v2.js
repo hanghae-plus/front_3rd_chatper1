9f8e6824dc1473206abc366c1ed2b908685a5d40
백광현 <bgh9651@gmail.com>
2024-10-05 13:48:51 +0900

    refactor: 주석 제거

--- src/lib/createElement__v2.js
+++ src/lib/createElement__v2.js
@@ -2,18 +2,14 @@ import { addEvent } from './eventManager';
 import { processVNode } from './renderElement';
 
 export function createElement__v2(vNode) {
-  // 이 함수는 createElement의 개선된 버전입니다.
-  // 1. falsy vNode 처리
   if (!vNode) {
     return document.createTextNode('');
   }
 
-  // 2. 문자열 또는 숫자 vNode 처리
   if (typeof vNode === 'string' || typeof vNode === 'number') {
     return document.createTextNode(vNode);
   }
 
-  // 3. 배열 vNode 처리 (DocumentFragment 사용)
   if (Array.isArray(vNode)) {
     const fragment = document.createDocumentFragment();
     vNode.forEach((child) => {
@@ -22,17 +18,14 @@ export function createElement__v2(vNode) {
     return fragment;
   }
 
-  // 4. 일반 요소 vNode 처리:
   if (!vNode.type) {
     return document.createTextNode('');
   }
 
-  //    - 요소 생성
   const element = document.createElement(vNode.type);
 
   element.vProps = vNode.props || {};
 
-  //    - 속성 설정 (이벤트 함수를 이벤트 위임 방식으로 등록할 수 있도록 개선)
   for (const [key, value] of Object.entries(vNode.props || {})) {
     if (key.startsWith('on') && typeof value === 'function') {
       const eventType = key.slice(2).toLowerCase();
@@ -44,7 +37,6 @@ export function createElement__v2(vNode) {
     }
   }
 
-  //    - 자식 요소 추가
   (vNode.children || []).forEach((child) => {
     element.appendChild(createElement__v2(child));
   });
